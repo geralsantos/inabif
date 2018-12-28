14956a49a64f28c01f16b1fb8fcaf3fe94c0607b
SYS Geral Poma Santos <gpoma@peruvian.local>
2018-12-28 15:23:12 -0500

    update

--- app/modulo/acceso/vista.acceso.geral.php
+++ app/modulo/acceso/vista.acceso.geral.php
@@ -116,28 +116,30 @@ public function executeQuery($query, $params=NULL){
 $x = new PG();
 $mdl = new mdl();
 //$mdl->createTable("DESCRIBE modulos"); 
-$x->dropTable("drop table CarDatosAdmision");
-$mdl->createTable ("create table CarDatosAdmision
+$x->dropTable("drop table CarCondicionIngreso");
+$mdl->createTable ("create table CarCondicionIngreso
 (
-Admision_Id int not null primary key,
+Condicion_Id int not null primary key,
 Tipo_Centro_Id  int,
-Periodo_Mes int,
-Periodo_Anio int,
-Residente_Id int,
-Mov_Poblacional varchar(100),
-Fecha_Ingreso 	date,
-Fecha_Reingreso	date,
-Institucion_derivado int,
-Motivo_Ingreso 	 varchar(100),
-Tipo_Documento	int,
+DNI	char(2),
+Tipo_Documento 		int,
 Numero_Documento	int,
-Estado			int,
-Fecha_Creacion		date,
+Lee_Escribe		char(2),
+Nivel_Educativo		int,
+Institucion_Educativa	int,
+Tipo_Seguro		int,
+Clasficacion_Socioeconomica	int,
+Familiares		char(2),
+Parentesco		int,
+Posee_Pension char(2),
+Tipo_Pension INT,
+Problematica_Familiar int,
+ Estado			int,
+Fecha_Creacion		TIMESTAMP DEFAULT SYSDATE,
 Fecha_Edicion		TIMESTAMP DEFAULT SYSDATE,
 Usuario_Crea		int,
 Usuario_Edita		int
-)
-");/*
+)");/*
 print_r($x->executeQuery("delete from modulos"));
 $arr = ["insert into modulos (id,centro_id,encargado_id,parent_id,url_template,icon,nombre,estado_completo, estado,fecha_creacion,usuario_creacion,usuario_edicion)
 values(1,1,1,0,'ppd-datos-actividades-tecnico-productivas','fa fa-laptop','ACOGIDA',0,1,'18-DEC-28',1,1)",
