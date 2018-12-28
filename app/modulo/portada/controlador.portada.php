d1dd3ebb59fa0fd64e1f1a24e18ae610bccbf870
SYS Geral Poma Santos <gpoma@peruvian.local>
2018-12-28 11:46:30 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -30,8 +30,8 @@ class portada extends App{
     public function buildTree($elements, $parentId = 0) {
       $branch = array();
       foreach ($elements as $element) {
-          if ($element['parent_id'] == $parentId) {
-              $children = $this->buildTree($elements, $element['id']);
+          if ($element['PARENT_ID'] == $parentId) {
+              $children = $this->buildTree($elements, $element['ID']);
               if ($children) {
                   $element['children'] = $children;
               }
