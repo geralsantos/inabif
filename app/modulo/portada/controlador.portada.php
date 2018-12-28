39a94ebd7975e0a9c184e327c19d9c2f86846772
SYS Geral Poma Santos <gpoma@peruvian.local>
2018-12-28 12:31:15 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -50,12 +50,12 @@ class portada extends App{
     foreach ($elements as $element)
     {
      // if (in_array($_SESSION["nivelusuario"],(explode(',',$element["niveles"])))) {
-        $li = $li  . (isset($element['children']) ? ($element['PARENT_ID']==0?'<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true">
+        $li = $li  . (isset($element['children']) ? ($element['PARENT_ID']==0?('<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true">
         <i class="menu-icon ' . $element["ICON"] . '"></i>' . $element['NOMBRE'] .'
       </a>
       <ul class="sub-menu children dropdown-menu">
       '. $this->buildTreeHtml($element['children'],'childs').'
-      </ul>':'<li class="menu-item-has-children dropdown">
+      </ul>'):('<li class="menu-item-has-children dropdown">
       <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true">
         <i class="menu-icon ' . $element["ICON"] . '"></i>' . $element['NOMBRE'] .'
       </a>
@@ -63,7 +63,7 @@ class portada extends App{
       '. $this->buildTreeHtml($element['children'],'childs').'
       </ul>
       </li>
-    '):
+    ')):
                           ('<li data-url="'.$element['URL_TEMPLATE'].'">
                             <i class="'.$element["ICON"].'"></i>
                             <a style="font-size:1em;" href="#'.$element['URL_TEMPLATE'].'" class=""> '.$element['NOMBRE'].'</a>
