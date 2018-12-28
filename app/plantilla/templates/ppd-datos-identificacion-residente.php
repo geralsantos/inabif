509510f7b175516fa15871995b03112b6a98dbe6
lvillalobos <lvillalobos@aeonlinesolutions.com>
2018-12-28 16:03:12 -0500

    update

--- app/plantilla/templates/ppd-datos-identificacion-residente.php
+++ app/plantilla/templates/ppd-datos-identificacion-residente.php
@@ -8,17 +8,17 @@
                 </div>
                 <div class="card-body card-block">
                     <form  class="form-horizontal"  v-on:submit.prevent="guardar">
-                    <div class="row">
+                        <div class="row">
                             <div class="form-group col-md-7">
                                 <label for="text-input" class=" form-control-label">Nombre Residente</label>
                                 <div class="autocomplete">
-                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()"/>
+                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
                                     <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
                                         <li class="loading" v-if="isLoading">
                                             Loading results...
                                         </li>
-                                        <li  @click="actualizar(coincidencia.id)" class="autocomplete-result" v-for="coincidencia in coincidencias">
-                                            {{coincidencia.nombre}}
+                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
+                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO}} - {{coincidencia.DOCUMENTO}}
                                         </li>
 
                                     </ul>
@@ -26,7 +26,7 @@
                             </div>
                             <div class="form-group col-md-2">
                                 <label for="text-input" class=" form-control-label">Año</label>
-                                <select name="select" id="anio"  v-model="anio" class="form-control">
+                                <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
                                 <option value="2018">2018</option>
                                 <option value="2019">2019</option>
                                 <option value="2020">2020</option>
@@ -36,7 +36,7 @@
                             </div>
                             <div class="form-group col-md-3">
                                 <div class=""><label for="text-input" class=" form-control-label">Mes</label>
-                                <select id="mes" v-model="mes" class="form-control" >
+                                <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
                                         <option value="1">Enero</option>
                                         <option value="2">Febrero</option>
                                         <option value="3">Marzo</option>
@@ -56,61 +56,53 @@
                         <div class="row">
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Apellido Paterno</label>
-                                <input type="text" v-model="txtaccionprogramada" name="txtaccionprogramada" placeholder="" class="form-control"> </div>
+                                <input type="text" v-model="Ape_Paterno" name="Ape_Paterno" placeholder="" class="form-control"> </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Apellido Materno</label>
-                                <input type="text" v-model="txtaccionprogramada" name="txtaccionprogramada" placeholder="" class="form-control"> </div>
+                                <input type="text" v-model="Ape_Materno" name="Ape_Materno" placeholder="" class="form-control"> </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Nombre</label>
-                                <input type="text" v-model="txtaccionprogramada" name="txtaccionprogramada" placeholder="" class="form-control"> </div>
+                                <input type="text" v-model="Nom_Usuario" name="Nom_Usuario" placeholder="" class="form-control"> </div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">País de Procedencia</label>
-                                <select name="select" v-model="select" class="form-control">
-                                    <option value="">País</option>
+                                <select name="Pais_Procencia" v-model="Pais_Procencia" class="form-control" @change="buscar_departamentos()">
+                                    <option v-for="pais in paises" :value="pais.ID">{{pais.NOMBRE}}</option>
                                 </select>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Departamento de Procedencia</label>
-                                <select name="select" v-model="select" class="form-control">
-                                    <option value="">Departamento</option>
+                                <select name="Depatamento_Procedencia" v-model="Depatamento_Procedencia" @change="buscar_provincias()" class="form-control">
+                                    <option v-for="departamento in departamentos" :value="departamento.ID">{{departamento.NOMBRE}}</option>
                                 </select>
                                 </div>
                             </div>
-                            <div class="form-group col-md-4">
-                                <div class=" "><label for="text-input" class=" form-control-label">Departamento de Nacimiento</label>
-                                <select name="select" v-model="select" class="form-control">
-                                    <option value="">Departamento</option>
-                                </select>
-                                </div>
-                            </div>
-
                         </div>
                         <div class="row">
-                        <div class="form-group col-md-4">
+                            <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Provincia de Procedencia</label>
-                                <select name="select" v-model="select" class="form-control">
-                                    <option value="">Provincia</option>
+                                <select name="Provincia_Procedencia" v-model="Provincia_Procedencia" @change="buscar_distritos()" class="form-control">
+                                    <option v-for="provincia in provincias" :value="provincia.ID">{{provincia.NOMBRE}}</option>
                                 </select>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Distrito de Procedencia</label>
-                                <select name="select" v-model="select" class="form-control">
-                                    <option value="">Distrito</option>
+                                <select name="Distrito_Procedencia" v-model="Distrito_Procedencia" class="form-control">
+                                    <option v-for="distrito in distritos" :value="distrito.ID">{{distrito.NOMBRE}}</option>
                                 </select>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Sexo</label>
-                                <select name="select" v-model="select" class="form-control">
-                                <option value="Hombre">Hombre</option>
-                                <option value="Mujer">Mujer</option>
+                                <select name="Sexo" v-model="Sexo" class="form-control">
+                                <option value="h">Hombre</option>
+                                <option value="m">Mujer</option>
                                 </select>
                                 </div>
                             </div>
@@ -118,30 +110,22 @@
                         <div class="row">
                         <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Fecha de Nacimiento</label>
-                                <input type="date" class="form-control" v-model=""  placeholder="DD-MM-YYYY" v-model="fecha_fin_real"  data-language='es'  />
+                                <input type="date" class="form-control" v-model="Fecha_Nacimiento"  placeholder="DD-MM-YYYY"  data-language='es'  />
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Edad</label>
-                                <input type="text" v-model="txtaccionprogramada" name="txtaccionprogramada" placeholder="" class="form-control">
+                                <input type="text" v-model="Edad" name="Edad" placeholder="" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Lengua Materna</label>
-                                <select name="select" v-model="select" class="form-control">
-                                <option value="Quechua">Quechua</option>
-                                <option value="Aymará">Aymará</option>
-                                <option value="Aymará">Aymará</option>
-                                <option value="Otra lengua nativa">Otra lengua nativa</option>
-                                <option value="Castellano">Castellano</option>
-                                <option value="Portugués">Portugués</option>
+                                <select name="Lengua_Materna" v-model="Lengua_Materna" class="form-control">
+                                    <option v-for="lengua in lenguas" :value="lengua.ID">{{lengua.NOMBRE}}</option>
                                 </select>
                                 </div>
                             </div>
-
                         </div>
-
-
                         <div class="row">
                             <div class="col-md-12 text-center" >
                                 <button type="submit" class="btn btn-success btn-sm">
---
+++ js/componentes/portada/ppd-datos-identificacion-residente.js
@@ -0,0 +1,169 @@
+Vue.component('ppd-datos-identificacion-residente', {
+    template:'#ppd-datos-identificacion-residente',
+    data:()=>({
+        Ape_Paterno:null,
+        Ape_Materno:null,
+        Nom_Usuario:null,
+        Pais_Procencia:null,
+        Depatamento_Procedencia:null,
+        Provincia_Procedencia:null,
+        Distrito_Procedencia:null,
+        Sexo:null,
+        Fecha_Nacimiento:null,
+        Edad:null,
+        Lengua_Materna:null,
+
+        paises:[],
+        departamentos:[],
+        provincias:[],
+        distritos:[],
+        lenguas:[],
+
+        nombre_residente:null,
+        isLoading:false,
+        mes:moment().format("MM"),
+        anio:(new Date()).getFullYear(),
+        coincidencias:[],
+        bloque_busqueda:false,
+        id_residente:null
+
+
+    }),
+    created:function(){
+    },
+    mounted:function(){
+        this.buscar_paises();
+        this.buscar_lenguas();
+    },
+    updated:function(){
+    },
+    methods:{
+        guardar(){
+            if (this.id_residente==null) {
+                swal('Error', 'Residente no existe', 'success');
+                return false;
+            }
+            let valores = {
+                Identificaion_Id: 1,
+                Ape_Paterno: this.Ape_Paterno,
+                Ape_Materno: this.Ape_Materno,
+                Nom_Usuario: this.Nom_Usuario,
+                Pais_Procencia: this.Pais_Procencia,
+                Depatamento_Procedencia: this.Depatamento_Procedencia,
+                Provincia_Procedencia: this.Provincia_Procedencia,
+                Distrito_Procedencia: this.Distrito_Procedencia,
+                Sexo: this.Sexo,
+                Fecha_Nacimiento:  moment(this.Fecha_Nacimiento, "YYYY-MM-DD").format("YY-MMM-DD"),
+                Edad: this.Edad,
+                Lengua_Materna: this.Lengua_Materna,
+
+                Residente_Id: this.id_residente,
+                Periodo_Mes: moment().format("MM"),
+                Periodo_Anio:moment().format("YYYY")
+
+                }
+
+            this.$http.post('insertar_datos?view',{tabla:'CarIdentificacionUsuario', valores:valores}).then(function(response){
+
+                if( response.body.resultado ){
+                    swal('', 'Registro Guardado', 'success');
+
+                }else{
+                  swal("", "Un error ha ocurrido", "error");
+                }
+            });
+        },
+        buscar_residente(){
+            this.id_residente = null;
+
+            var word = this.nombre_residente;
+            if( word.length >= 4){
+                this.coincidencias = [];
+                this.bloque_busqueda = true;
+                this.isLoading = true;
+
+                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
+
+                    if( response.body.data != undefined){
+                        this.isLoading = false;
+                        this.coincidencias = response.body.data;
+                    }else{
+                        this.bloque_busqueda = false;
+                        this.isLoading = false;
+                        this.coincidencias = [];
+                    }
+                 });
+            }else{
+                this.bloque_busqueda = false;
+                this.isLoading = false;
+                this.coincidencias = [];
+            }
+        },
+        actualizar(coincidencia){
+            this.id_residente = coincidencia.ID;
+            this.nombre_residente=coincidencia.NOMBRE;
+            this.coincidencias = [];
+            this.bloque_busqueda = false;
+
+            this.$http.post('cargar_datos_residente?view',{tabla:'CarIdentificacionUsuario', residente_id:this.id_residente }).then(function(response){
+
+                if( response.body.atributos != undefined){
+                    this.Ape_Paterno = response.body.atributos[0]["APE_PATERNO"];
+                    this.Ape_Materno = response.body.atributos[0]["APE_MATERNO"];
+                    this.Nom_Usuario = response.body.atributos[0]["NOM_USUARIO"];
+                    this.Pais_Procencia = response.body.atributos[0]["PAIS_PROCENCIA"];
+                    this.Depatamento_Procedencia = response.body.atributos[0]["DEPATAMENTO_PROCEDENCIA"];
+                    this.Provincia_Procedencia = response.body.atributos[0]["PROVINCIA_PROCEDENCIA"];
+                    this.Distrito_Procedencia = response.body.atributos[0]["DISTRITO_PROCEDENCIA"];
+                    this.Sexo = response.body.atributos[0]["SEXO"];
+                    this.Fecha_Nacimiento = response.body.atributos[0]["FECHA_NACIMIENTO"];
+                    this.Edad = response.body.atributos[0]["EDAD"];
+                    this.Lengua_Materna = response.body.atributos[0]["LENGUA_MATERNA"];
+                }
+             });
+
+        },
+        buscar_paises(){
+            this.$http.post('buscar?view',{tabla:''}).then(function(response){
+                if( response.body.data ){
+                    this.paises= response.body.data;
+                }
+
+            });
+        },
+
+        buscar_departamentos(){
+            this.$http.post('buscar?view',{tabla:''}).then(function(response){
+                if( response.body.data ){
+                    this.departamentos= response.body.data;
+                }
+
+            });
+        },
+        buscar_provincias(){
+            this.$http.post('buscar?view',{tabla:''}).then(function(response){
+                if( response.body.data ){
+                    this.provincias= response.body.data;
+                }
+
+            });
+        },
+        buscar_distritos(){
+            this.$http.post('buscar?view',{tabla:''}).then(function(response){
+                if( response.body.data ){
+                    this.distritos= response.body.data;
+                }
+
+            });
+        },
+        buscar_lenguas(){
+            this.$http.post('buscar?view',{tabla:''}).then(function(response){
+                if( response.body.data ){
+                    this.lenguas= response.body.data;
+                }
+
+            });
+        },
+
+    }
+  })
