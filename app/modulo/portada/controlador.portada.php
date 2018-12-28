73faad2e3b0b01993ef210a98e960ea6d3efc6c8
SYS Geral Poma Santos <gpoma@peruvian.local>
2018-12-28 17:56:41 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -60,14 +60,14 @@ class portada extends App{
         </div>
     </div>*/ 
     $li = $li  . (isset($element['children']) ? ('
-    <a href="#'.$element['PARENT_ID'].'" class="list-group-item level-0" data-toggle="collapse" aria-expanded="true" id="gardening">'.$element['NOMBRE'].' 
+    <a href="#node'.$element['PARENT_ID'].'" class="list-group-item level-0" data-toggle="collapse" aria-expanded="true" id="gardening">'.$element['NOMBRE'].' 
         <i class="fa fa-caret-down"></i>
     </a>
-    <div class="collapse" id="'.$element['PARENT_ID'].'">
+    <div class="collapse" id="node'.$element['PARENT_ID'].'">
     '. $this->buildTreeHtml($element['children'],'childs').'
     </div>
       ') :
-        ('<div class="collapse" id="'.$element['ID'].'">
+        ('<div class="collapse" id="node'.$element['ID'].'">
         <a href="'.$element['URL_TEMPLATE'].'" class="list-group-item level-2" id="moss-control">'.$element['NOMBRE'].'</a>
     </div>') ) ;
      // if (in_array($_SESSION["nivelusuario"],(explode(',',$element["niveles"])))) {
