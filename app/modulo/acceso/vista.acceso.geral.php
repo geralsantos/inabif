1dd3727cb9bc5254ba60f394adc46812757fa414
SYS Geral Poma Santos <gpoma@peruvian.local>
2018-12-28 10:29:20 -0500

    update

--- app/modulo/acceso/vista.acceso.geral.php
+++ app/modulo/acceso/vista.acceso.geral.php
@@ -114,8 +114,7 @@ public function executeQuery($query, $params=NULL){
 
 $x = new PG();
 $mdl = new mdl();
-$x->dropTable("drop table modulos");
-/*
+//$x->dropTable("drop table modulos");
 $mdl->createTable ("create table modulos (
     id INT NOT NULL primary key,
     centro_id INT NOT NULL,
@@ -130,7 +129,7 @@ $mdl->createTable ("create table modulos (
     fecha_edicion TIMESTAMP DEFAULT SYSDATE,
     usuario_creacion INT NOT NULL,
     usuario_edicion INT NOT NULL
-    )");*/
+    )");
 //print_r($x->executeQuery("delete from CarActividades"));
 /*$x->insertData('modulos', array("id"=>1,"centro_id"=>1,"encargado_id"=>1,"parent_id"=>1,"url_template"=>'ppd-datos-actividades',"icon"=>'fa fa-laptop',"nombre"=>'ACOGIDA',"estado_completo"=>0,"estado"=>1,"fecha_creacion"=>'18-DEC-28',"usuario_creacion"=>1,"usuario_edicion"));*/
 /*$x->executeQuery("drop table modulos");
