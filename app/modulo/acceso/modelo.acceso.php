fe64dea86c355f7e63974d299c10649a28622917
geral <geral_366@hotmail.com>
2018-12-27 20:16:00 -0500

    update

--- app/modulo/acceso/modelo.acceso.php
+++ app/modulo/acceso/modelo.acceso.php
@@ -2,7 +2,7 @@
 class modeloAcceso extends MySQL{
 
     public function getSesion($usuario, $clave){
-        $usuario = $this->executeQuery("select us.* from kpi_usuarios us where us.username=:clave and us.clave=:clave",array("usuario"=>$usuario,"clave"=>$clave));
+        $usuario = $this->executeQuery("select us.* from usuarios us where us.usuario=:usuario and us.clave=:clave",array("usuario"=>$usuario,"clave"=>$clave));
         return $usuario;
     }
 }
