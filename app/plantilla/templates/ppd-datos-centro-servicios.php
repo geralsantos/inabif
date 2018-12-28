cff5eb5daa880309b3769ad434075c2c509c4c85
lvillalobos <lvillalobos@aeonlinesolutions.com>
2018-12-28 11:58:07 -0500

    update

--- app/plantilla/templates/ppd-datos-centro-servicios.php
+++ app/plantilla/templates/ppd-datos-centro-servicios.php
@@ -8,17 +8,17 @@
                 </div>
                 <div class="card-body card-block">
                     <form class="form-horizontal" v-on:submit.prevent="guardar">
-                    <div class="row">
+                        <div class="row">
                             <div class="form-group col-md-7">
                                 <label for="text-input" class=" form-control-label">Nombre Residente</label>
                                 <div class="autocomplete">
-                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()"/>
+                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
                                     <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
                                         <li class="loading" v-if="isLoading">
                                             Loading results...
                                         </li>
-                                        <li  @click="actualizar(coincidencia.id)" class="autocomplete-result" v-for="coincidencia in coincidencias">
-                                            {{coincidencia.nombre}}
+                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
+                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO}} - {{coincidencia.DOCUMENTO}}
                                         </li>
 
                                     </ul>
@@ -26,7 +26,7 @@
                             </div>
                             <div class="form-group col-md-2">
                                 <label for="text-input" class=" form-control-label">Año</label>
-                                <select name="select" id="anio"  v-model="anio" class="form-control">
+                                <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
                                 <option value="2018">2018</option>
                                 <option value="2019">2019</option>
                                 <option value="2020">2020</option>
@@ -36,7 +36,7 @@
                             </div>
                             <div class="form-group col-md-3">
                                 <div class=""><label for="text-input" class=" form-control-label">Mes</label>
-                                <select id="mes" v-model="mes" class="form-control" >
+                                <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
                                         <option value="1">Enero</option>
                                         <option value="2">Febrero</option>
                                         <option value="3">Marzo</option>
@@ -86,44 +86,51 @@
                         <div class="row">
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Departamento del Centro de Atención</label>
-                                <select name="CarDepart" v-model="CarDepart" class="form-control">
-                                    <option value="">Departamento</option>
+                                <select name="CarDepart" v-model="CarDepart" class="form-control"  @change="cargar_provincias()">
+                                    <option v-for="departamento in departamentos" :value="departamento.CODDEPT">{{departamento.NOMDEPT}}</option>
                                 </select>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Provincia del Centro de Atención</label>
-                                <select name="CarProv" v-model="CarProv" class="form-control">
-                                    <option value="">Provincia</option>
+                                <select name="CarProv" v-model="CarProv" class="form-control"  @change="cargar_distritos()">
+                                    <option v-for="provincia in provincias" :value="provincia.CODPROV">{{provincia.NOMPROV}}</option>
                                 </select>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Distrito del Centro de Atención</label>
                                 <select name="CarDistrito" v-model="CarDistrito" class="form-control">
-                                    <option value="">Distrito</option>
+                                    <option v-for="distrito in distritos" :value="distrito.CODDIST">{{distrito.CODDIST}}</option>
                                 </select>
                                 </div>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="form-group col-md-4">
+                            <div class="form-group col-md-6">
                                 <div class=""><label for="text-input" class=" form-control-label">Área de residencia del centro de atención</label>
                                     <select name="areaResidencia" v-model="areaResidencia" class="form-control">
-                                    <option value="">URBANO</option>
-                                    <option value="">RURAL</option>
+                                    <option value="Urbano">Urbano</option>
+                                    <option value="Rural">Rural</option>
                                 </select></div>
                             </div>
-                            <div class="form-group col-md-3">
+                            <div class="form-group col-md-6">
+                                <div class=""><label for="text-input" class=" form-control-label">Centro Poblado del Centro de Atención</label>
+                                <input type="text" v-model="centroPoblado" name="centroPoblado" placeholder="" class="form-control"></div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+
+                            <div class="form-group col-md-6">
                                 <div class=""><label for="text-input" class=" form-control-label">Código del Centro de Atención</label>
                                 <input type="text" v-model="codigoCentroAtencion" name="codigoCentroAtencion" placeholder="" class="form-control"></div>
                             </div>
-                            <div class="form-group col-md-5">
+                            <div class="form-group col-md-6">
                                 <div class=""><label for="text-input" class=" form-control-label">Nombre del Centro de Atención</label>
                                 <input type="text" v-model="nombreCentroAtencion" name="nombreCentroAtencion" placeholder="" class="form-control"></div>
                             </div>
                         </div>
-
                         <div class="row">
                             <div class="col-md-12 text-center" >
                                 <button type="submit" class="btn btn-success btn-sm">
--- js/componentes/portada/ppd-datos-centro-servicios.js
+++ js/componentes/portada/ppd-datos-centro-servicios.js
@@ -10,33 +10,57 @@ Vue.component('ppd-datos-centro-servicios', {
         CarDepart:null,
         CarProv:null,
         areaResidencia:null,
+        centroPoblado:null,
         CarDistrito:null,
         codigoCentroAtencion:null,
         nombreCentroAtencion:null,
+
+        departamentos:[],
+        provincias:[],
+        distritos:[],
+
+        nombre_residente:null,
+        isLoading:false,
+        mes:moment().format("MM"),
+        anio:(new Date()).getFullYear(),
+        coincidencias:[],
+        bloque_busqueda:false,
+        id_residente:null
 
     }),
     created:function(){
     },
     mounted:function(){
+        this.cargar_departamentos();
     },
     updated:function(){
     },
     methods:{
         guardar(){
-            let valores = { CarCodEntidad:this.CarCodEntidad,
-                CarNomEntidad:this.CarNomEntidad,
-                CarCodLinea:this.CarCodLinea,
-                CarLineaI:this.CarLineaI,
-                CarCodServicio:this.CarCodServicio,
-                CarNomServicio:this.CarNomServicio,
-                CarDepart:this.CarDepart,
-                CarProv:this.CarProv,
-                areaResidencia:this.areaResidencia,
-                CarDistrito:this.CarDistrito,
-                codigoCentroAtencion:this.codigoCentroAtencion,
-                nombreCentroAtencion:this.nombreCentroAtencion,
+            if (this.id_residente==null) {
+                swal('Error', 'Residente no existe', 'success');
+                return false;
+            }
+            let valores = {
+                Cod_Entidad:this.CarCodEntidad,
+                Nom_Entidad:this.CarNomEntidad,
+                Cod_Linea:this.CarCodLinea,
+                Linea_Intervencion:this.CarLineaI,
+                Cod_Servicio:this.CarCodServicio,
+                Nom_Servicio:this.CarNomServicio,
+                Ubigeo_Ine: this.CarDepart+this.CarProv+this.CarDistrito,
+                Departamento_CAtencion:this.CarDepart,
+                Provincia_CAtencion:this.CarProv,
+                Distrito_CAtencion:this.CarDistrito,
+                Centro_Poblado:this.centroPoblado,
+                Centro_Residencia:this.areaResidencia,
+                Cod_CentroAtencion:this.codigoCentroAtencion,
+                Nom_CentroAtencion:this.nombreCentroAtencion,
+                Residente_Id: this.id_residente,
+                Periodo_Mes: moment().format("MM"),
+                Periodo_Anio:moment().format("YYYY")
                         }
-            this.$http.post('insertar_datos?view',{tabla:'', valores:valores}).then(function(response){
+            this.$http.post('insertar_datos?view',{tabla:'CarCentroServicio', valores:valores}).then(function(response){
 
                 if( response.body.resultado ){
                     swal('', 'Registro Guardado', 'success');
@@ -45,6 +69,90 @@ Vue.component('ppd-datos-centro-servicios', {
                   swal("", "Un error ha ocurrido", "error");
                 }
             });
-        }
+        },
+        buscar_residente(){
+            this.id_residente = null;
+
+            var word = this.nombre_residente;
+            if( word.length >= 4){
+                this.coincidencias = [];
+                this.bloque_busqueda = true;
+                this.isLoading = true;
+
+                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
+
+                    if( response.body.data != undefined){
+                        this.isLoading = false;
+                        this.coincidencias = response.body.data;
+                    }else{
+                        this.bloque_busqueda = false;
+                        this.isLoading = false;
+                        this.coincidencias = [];
+                    }
+                 });
+            }else{
+                this.bloque_busqueda = false;
+                this.isLoading = false;
+                this.coincidencias = [];
+            }
+        },
+        actualizar(coincidencia){
+            this.id_residente = coincidencia.ID;
+            this.nombre_residente=coincidencia.NOMBRE;
+            this.coincidencias = [];
+            this.bloque_busqueda = false;
+
+            this.$http.post('cargar_datos_residente?view',{tabla:'CarCentroServicio', residente_id:this.id_residente }).then(function(response){
+
+                if( response.body.atributos != undefined){
+
+                    this.CarCodEntidad = response.body.atributos[0]["COD_ENTIDAD"];
+                    this.CarNomEntidad = response.body.atributos[0]["NOM_ENTIDAD"];
+                    this.CarCodLinea = response.body.atributos[0]["COD_LINEA"];
+                    this.CarLineaI = response.body.atributos[0]["LINEA_INTERVENCION"];
+                    this.CarCodServicio = response.body.atributos[0]["COD_SERVICIO"];
+                    this.CarNomServicio = response.body.atributos[0]["NOM_SERVICIO"];
+                    this.CarDepart = response.body.atributos[0]["DEPARTAMENTO_CATENCION"];
+                    this.CarProv = response.body.atributos[0]["PROVINCIA_CATENCION"];
+                    this.CarDistrito = response.body.atributos[0]["DISTRITO_CATENCION"];
+                    this.centroPoblado = response.body.atributos[0]["CENTRO_POBLADO"];
+                    this.areaResidencia = response.body.atributos[0]["CENTRO_RESIDENCIA"];
+                    this.codigoCentroAtencion = response.body.atributos[0]["COD_CENTROATENCION"];
+                    this.nombreCentroAtencion = response.body.atributos[0]["NOM_CENTROATENCION"];
+
+                }
+             });
+
+        },
+        cargar_departamentos(){
+            this.departamentos=[];
+            this.$http.post('cargar_departamentos?view',{}).then(function(response){
+                if( response.body.data != undefined){
+                    this.departamentos= response.body.data;
+                    this.cargar_provincias();
+                }
+             });
+        },
+        cargar_provincias(){
+
+            this.provincias=[];
+            let cod = this.CarDepart;
+            this.$http.post('cargar_provincias?view',{cod:cod}).then(function(response){
+                if( response.body.data != undefined){
+                    this.provincias= response.body.data;
+                    this.cargar_distritos();
+                }
+            });
+        },
+        cargar_distritos(){
+
+            this.distritos=[];
+            let cod = this.CarProv;
+            this.$http.post('cargar_departamentos?view',{cod:cod}).then(function(response){
+                if( response.body.data != undefined){
+                    this.distritos= response.body.data;
+                }
+             });
+        },
     }
   })
