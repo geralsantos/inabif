f3dd0ce008f6e4a7173900edd7c335fb5b2428a0
lvillalobos <lvillalobos@aeonlinesolutions.com>
2018-12-28 13:18:38 -0500

    update

--- js/componentes/portada/ppd-datos-egreso-educacion.js
+++ js/componentes/portada/ppd-datos-egreso-educacion.js
@@ -9,23 +9,31 @@ Vue.component('ppd-datos-egreso-educacion', {
         CarAsistenciaEscolar:null,
         CarDesempeAcademico:null,
 
+        academicos: [],
+
         nombre_residente:null,
         isLoading:false,
         mes:moment().format("MM"),
         anio:(new Date()).getFullYear(),
         coincidencias:[],
-        bloque_busqueda:false
+        bloque_busqueda:false,
+        id_residente:null
     }),
     created:function(){
     },
     mounted:function(){
+        this.buscar_desempeno_academico();
     },
     updated:function(){
     },
     methods:{
         guardar(){
-            let valores = {
+            if (this.id_residente==null) {
+                swal('Error', 'Residente no existe', 'success');
+                return false;
+            }
 
+            let valores = {
                 Plan_Educacion: this.CarIntervencion,
                 Meta_PII: this.CarDesMeta,
                 Informe_Tecnico: this.CarInformeEvolutivo,
@@ -33,13 +41,11 @@ Vue.component('ppd-datos-egreso-educacion', {
                 Cumple_Plan: this.CarCumplimientoPlan,
                 Asistencia_Escolar: this.CarAsistenciaEscolar,
                 Desenpeno: this.CarDesempeAcademico,
+                Residente_Id: this.id_residente,
+                Periodo_Mes: moment().format("MM"),
+                Periodo_Anio:moment().format("YYYY")
 
-
-                id_residente: this.id_residente,
-                Periodo_Mes: this.mes,
-                Periodo_Anio:this.anio
-
-                        }
+                }
             this.$http.post('insertar_datos?view',{tabla:'CarEgresoEducacion', valores:valores}).then(function(response){
 
                 if( response.body.resultado ){
@@ -53,12 +59,13 @@ Vue.component('ppd-datos-egreso-educacion', {
         buscar_residente(){
             this.id_residente = null;
 
-            let word = this.nombre_residente;
+            var word = this.nombre_residente;
             if( word.length >= 4){
                 this.coincidencias = [];
                 this.bloque_busqueda = true;
                 this.isLoading = true;
-                this.$http.post('ejecutar_consulta?view',{tabla:'', campo:'coincidencia', like:word }).then(function(response){
+
+                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
 
                     if( response.body.data != undefined){
                         this.isLoading = false;
@@ -75,26 +82,36 @@ Vue.component('ppd-datos-egreso-educacion', {
                 this.coincidencias = [];
             }
         },
-        actualizar(id){
-            this.id_editado = id;
+        actualizar(coincidencia){
+            this.id_residente = coincidencia.ID;
+            this.nombre_residente=coincidencia.NOMBRE;
             this.coincidencias = [];
             this.bloque_busqueda = false;
-            let where = {"id_residente": this.id_residente, "estado": 1}
-            this.$http.post('cargar_datos_residente?view',{tabla:'CarEgresoEducacion', where:where }).then(function(response){
+
+            this.$http.post('cargar_datos_residente?view',{tabla:'CarEgresoEducacion', residente_id:this.id_residente }).then(function(response){
 
                 if( response.body.atributos != undefined){
 
-                    this.CarIntervencion = response.body.atributos[0]["Plan_Educacion"];
-                    this.CarDesMeta = response.body.atributos[0]["Meta_PII"];
-                    this.CarInformeEvolutivo = response.body.atributos[0]["Informe_Tecnico"];
-                    this.CarDesInfome = response.body.atributos[0]["Des_Informe"];
-                    this.CarCumplimientoPlan = response.body.atributos[0]["Cumple_Plan"];
-                    this.CarAsistenciaEscolar = response.body.atributos[0]["Asistencia_Escolar"];
-                    this.CarDesempeAcademico = response.body.atributos[0]["Desenpeno"];
+                    this.CarIntervencion = response.body.atributos[0]["PLAN_EDUCACION"];
+                    this.CarDesMeta = response.body.atributos[0]["META_PII"];
+                    this.CarInformeEvolutivo = response.body.atributos[0]["INFORME_TECNICO"];
+                    this.CarDesInfome = response.body.atributos[0]["DES_INFORME"];
+                    this.CarCumplimientoPlan = response.body.atributos[0]["CUMPLE_PLAN"];
+                    this.CarAsistenciaEscolar = response.body.atributos[0]["ASISTENCIA_ESCOLAR"];
+                    this.CarDesempeAcademico = response.body.atributos[0]["DESENPENO"];
 
                 }
              });
 
         },
+        buscar_desempeno_academico(){
+            this.$http.post('buscar?view',{tabla:''}).then(function(response){
+                if( response.body.data ){
+                    this.academicos= response.body.data;
+                }
+
+            });
+        },
+
     }
   })
