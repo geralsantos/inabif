c2b602f5081570766ddb57cf0d127a85e863fb25
SYS Geral Poma Santos <gpoma@peruvian.local>
2018-12-28 10:38:31 -0500

    update

--- app/modulo/acceso/vista.acceso.geral.php
+++ app/modulo/acceso/vista.acceso.geral.php
@@ -99,7 +99,6 @@ public function executeQuery($query, $params=NULL){
                 $coma = ',';
             }
             $query .= '('.$queryKeys.') VALUES ('.$queryValues.')';
-            print_r($query);
             $stmt = parent::prepare($query);
             $stmt->execute($params);
             if($stmt->rowCount()>0){
@@ -132,7 +131,7 @@ $mdl = new mdl();
     usuario_creacion INT NOT NULL,
     usuario_edicion INT NOT NULL
     )");*/
-//print_r($x->executeQuery("delete from CarActividades"));
+print_r($x->executeQuery("delete from modulos"));
 $x->insertData('modulos', array("id"=>1,"centro_id"=>1,"encargado_id"=>1,"parent_id"=>1,"url_template"=>'ppd-datos-actividades',"icon"=>'fa fa-laptop',"nombre"=>'ACOGIDA',"estado_completo"=>0,"estado"=>1,"fecha_creacion"=>'18-DEC-28',"usuario_creacion"=>1,"usuario_edicion"=>1));
 print_r($x->executeQuery("select * from modulos"));
 /*$x->executeQuery("drop table modulos");
