8b6ea061b7b096c38720639afce70b3824f2186a
geral <geral_366@hotmail.com>
2018-12-27 23:48:23 -0500

    update

--- app/modulo/acceso/vista.acceso.geral.php
+++ app/modulo/acceso/vista.acceso.geral.php
@@ -53,6 +53,7 @@ public function executeQuery($query, $params=NULL){
     }
 
 $x = new PG();
+print_r($x->executeQuery("delete from CarActividades"));
 print_r($x->executeQuery("SELECT * FROM CARACTIVIDADES"));
 
  ?>
