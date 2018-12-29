7fca5002aa24c85ad6e4527913b82a72930616ac
geral <geral_366@hotmail.com>
2018-12-29 13:58:03 -0500

    update

--- app/modulo/acceso/vista.acceso.geral.php
+++ app/modulo/acceso/vista.acceso.geral.php
@@ -880,6 +880,7 @@ create sequence Carproblematica_familiar
 ','
 create sequence Cardesempeno_academico'];
 foreach ($arr as $key => $value) {
+    echo $key;
     $x->dropTable($arrdrop[$key]);
     $mdl->createTable($arrdropseq[$key]);
     $mdl->createTable($value);
