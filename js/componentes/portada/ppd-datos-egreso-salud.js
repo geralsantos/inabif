b17ac8f5de3f038d5f5970e84daab98b8a1c21af
geral <geral_366@hotmail.com>
2018-12-29 13:09:12 -0500

    update

--- js/componentes/portada/ppd-datos-egreso-salud.js
+++ js/componentes/portada/ppd-datos-egreso-salud.js
@@ -29,7 +29,7 @@ Vue.component('ppd-datos-egreso-salud', {
     methods:{
         guardar(){
             if (this.id_residente==null) {
-                swal('Error', 'Residente no existe', 'success');
+                swal('Error', 'Residente no existe', 'warning');
                 return false;
             }
             let valores = {
