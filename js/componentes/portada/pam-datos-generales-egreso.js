68202046d32623ffe6c95f50451b1f56a741d67a
geral <geral_366@hotmail.com>
2018-12-29 11:12:43 -0500

    update

--- js/componentes/portada/pam-datos-generales-egreso.js
+++ js/componentes/portada/pam-datos-generales-egreso.js
@@ -1,25 +1,7 @@
 Vue.component('pam-datos-generales-egreso', {
     template:'#pam-datos-generales-egreso',
     data:()=>({
-        Fecha_Egreso:null,
-        MotivoEgreso:null,
-        Retiro_Voluntario:null,
-        Reinsercion_Familiar:null,
-        Traslado_Entidad_Salud:null,
-        Traslado_Otra_Entidad:null,
-        Fallecimiento:null,
-        RestitucionAseguramientoSaludo:null,
-        Restitucion_Derechos_DNI:null,
-        RestitucionReinsercionFamiliar:null,
-              
-        nombre_residente:null,
-        isLoading:false,
-        mes:moment().format("MM"),
-        anio:(new Date()).getFullYear(),
-        coincidencias:[],
-        bloque_busqueda:false,
-        id_residente:null
-
+        
 
     }),
     created:function(){
