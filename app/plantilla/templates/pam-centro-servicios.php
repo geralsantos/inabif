86ca421549a24ac644e62379518d70b7f006e536
geralsantos <geral_366@hotmail.com>
2018-12-29 10:35:31 -0500

    update

---
+++ app/plantilla/templates/pam-centro-servicios.php
@@ -0,0 +1,146 @@
+<template id="pam-datos-centro-servicios">
+    <div class="content mt-3">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Datos del Centro de Servicios</strong>
+                    <h6>Formulario de Carga de Datos</h6>
+                </div>
+                <div class="card-body card-block">
+                    <form class="form-horizontal" v-on:submit.prevent="guardar">
+                        <div class="row">
+                            <div class="form-group col-md-7">
+                                <label for="text-input" class=" form-control-label">Nombre Residente</label>
+                                <div class="autocomplete">
+                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
+                                    <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
+                                        <li class="loading" v-if="isLoading">
+                                            Loading results...
+                                        </li>
+                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
+                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO}} - {{coincidencia.DOCUMENTO}}
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="text-input" class=" form-control-label">Año</label>
+                                <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                                <option value="2020">2020</option>
+                                <option value="2021">2021</option>
+                                </select>
+
+                            </div>
+                            <div class="form-group col-md-3">
+                                <div class=""><label for="text-input" class=" form-control-label">Mes</label>
+                                <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
+                                        <option value="1">Enero</option>
+                                        <option value="2">Febrero</option>
+                                        <option value="3">Marzo</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Mayo</option>
+                                        <option value="6">Junio</option>
+                                        <option value="7">Julio</option>
+                                        <option value="8">Agosto</option>
+                                        <option value="9">Septiembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Noviembre</option>
+                                        <option value="12">Diciembre</option>
+                                    </select> </div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Código de la Entidad</label>
+                                <input type="text" v-model="CarCodEntidad" name="CarCodEntidad" placeholder="" class="form-control"> </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class=" "><label for="text-input" class=" form-control-label">Nombre de la Entidad</label>
+                                <input type="text" v-model="CarNomEntidad" name="CarNomEntidad" placeholder="" class="form-control"> </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Código de la Línea</label>
+                                <input type="text" v-model="CarCodLinea" name="CarCodLinea" placeholder="" class="form-control"> </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class=" "><label for="text-input" class=" form-control-label">Línea de Intervención</label>
+                                <input type="text" v-model="CarLineaI" name="CarLineaI" placeholder="" class="form-control"> </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Código del Servicio</label>
+                                <input type="text" v-model="CarCodServicio" name="CarCodServicio" placeholder="" class="form-control"> </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class=" "><label for="text-input" class=" form-control-label">Nombre del Servicio</label>
+                                <input type="text" v-model="CarNomServicio" name="CarNomServicio" placeholder="" class="form-control"> </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Departamento del Centro de Atención</label>
+                                <select name="CarDepart" v-model="CarDepart" class="form-control"  @change="cargar_provincias()">
+                                    <option v-for="departamento in departamentos" :value="departamento.CODDEPT">{{departamento.NOMDEPT}}</option>
+                                </select>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Provincia del Centro de Atención</label>
+                                <select name="CarProv" v-model="CarProv" class="form-control"  @change="cargar_distritos()">
+                                    <option v-for="provincia in provincias" :value="provincia.CODPROV">{{provincia.NOMPROV}}</option>
+                                </select>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Distrito del Centro de Atención</label>
+                                <select name="CarDistrito" v-model="CarDistrito" class="form-control">
+                                    <option v-for="distrito in distritos" :value="distrito.CODDIST">{{distrito.CODDIST}}</option>
+                                </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <div class=""><label for="text-input" class=" form-control-label">Área de residencia del centro de atención</label>
+                                    <select name="areaResidencia" v-model="areaResidencia" class="form-control">
+                                    <option value="Urbano">Urbano</option>
+                                    <option value="Rural">Rural</option>
+                                </select></div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class=""><label for="text-input" class=" form-control-label">Centro Poblado del Centro de Atención</label>
+                                <input type="text" v-model="centroPoblado" name="centroPoblado" placeholder="" class="form-control"></div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+
+                            <div class="form-group col-md-6">
+                                <div class=""><label for="text-input" class=" form-control-label">Código del Centro de Atención</label>
+                                <input type="text" v-model="codigoCentroAtencion" name="codigoCentroAtencion" placeholder="" class="form-control"></div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class=""><label for="text-input" class=" form-control-label">Nombre del Centro de Atención</label>
+                                <input type="text" v-model="nombreCentroAtencion" name="nombreCentroAtencion" placeholder="" class="form-control"></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 text-center" >
+                                <button type="submit" class="btn btn-success btn-sm">
+                                    <i class="fa fa-send"></i> Grabar datos
+                                </button>
+                            </div>
+                        </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div> <!-- .content -->
+</template>
---
+++ js/componentes/portada/pam-datos-generales-egreso.js
@@ -0,0 +1,120 @@
+Vue.component('pam-datos-generales-egreso', {
+    template:'#pam-datos-generales-egreso',
+    data:()=>({
+        Fecha_Egreso:null,
+        MotivoEgreso:null,
+        Retiro_Voluntario:null,
+        Reinsercion_Familiar:null,
+        Traslado_Entidad_Salud:null,
+        Traslado_Otra_Entidad:null,
+        Fallecimiento:null,
+        RestitucionAseguramientoSaludo:null,
+        Restitucion_Derechos_DNI:null,
+        RestitucionReinsercionFamiliar:null,
+              
+        nombre_residente:null,
+        isLoading:false,
+        mes:moment().format("MM"),
+        anio:(new Date()).getFullYear(),
+        coincidencias:[],
+        bloque_busqueda:false,
+        id_residente:null
+
+
+    }),
+    created:function(){
+    },
+    mounted:function(){
+
+    },
+    updated:function(){
+    },
+    methods:{
+        guardar(){
+            if (this.id_residente==null) {
+                swal('Error', 'Residente no existe', 'success');
+                return false;
+            }
+            let valores = {
+                Id: 1,
+                Fecha_Egreso:this.Fecha_Egreso,
+                MotivoEgreso:this.MotivoEgreso,
+                Retiro_Voluntario:this.Retiro_Voluntario,
+                Reinsercion_Familiar:this.Reinsercion_Familiar,
+                Traslado_Entidad_Salud:this.Traslado_Entidad_Salud,
+                Traslado_Otra_Entidad:this.Traslado_Otra_Entidad,
+                Fallecimiento:this.Fallecimiento,
+                RestitucionAseguramientoSaludo:this.RestitucionAseguramientoSaludo,
+                Restitucion_Derechos_DNI:this.Restitucion_Derechos_DNI,
+                RestitucionReinsercionFamiliar:this.RestitucionReinsercionFamiliar,
+                
+                Residente_Id: this.id_residente,
+                Periodo_Mes: moment().format("MM"),
+                Periodo_Anio:moment().format("YYYY")
+
+            }
+                     
+            this.$http.post('insertar_datos?view',{tabla:'pam_EgresoUsuario', valores:valores}).then(function(response){
+
+                if( response.body.resultado ){
+                    swal('', 'Registro Guardado', 'success');
+
+                }else{
+                  swal("", "Un error ha ocurrido", "error");
+                }
+            });
+        },
+        buscar_residente(){
+            this.id_residente = null;
+
+            var word = this.nombre_residente;
+            if( word.length >= 4){
+                this.coincidencias = [];
+                this.bloque_busqueda = true;
+                this.isLoading = true;
+
+                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
+
+                    if( response.body.data != undefined){
+                        this.isLoading = false;
+                        this.coincidencias = response.body.data;
+                    }else{
+                        this.bloque_busqueda = false;
+                        this.isLoading = false;
+                        this.coincidencias = [];
+                    }
+                 });
+            }else{
+                this.bloque_busqueda = false;
+                this.isLoading = false;
+                this.coincidencias = [];
+            }
+        },
+        actualizar(coincidencia){
+            this.id_residente = coincidencia.ID;
+            this.nombre_residente=coincidencia.NOMBRE;
+            this.coincidencias = [];
+            this.bloque_busqueda = false;
+
+            this.$http.post('cargar_datos_residente?view',{tabla:'pam_EgresoUsuario', residente_id:this.id_residente }).then(function(response){
+
+                if( response.body.atributos != undefined){
+
+                    this.Fecha_Egreso = response.body.atributos[0]["FECHA_EGRESO"];
+                    this.MotivoEgreso = response.body.atributos[0]["MOTIVOEGRESO"];
+                    this.Retiro_Voluntario = response.body.atributos[0]["RETIRO_VOLUNTARIO"];
+                    this.Reinsercion_Familiar = response.body.atributos[0]["REINSERCION_FAMILIAR"];
+                    this.Traslado_Entidad_Salud = response.body.atributos[0]["TRASLADO_ENTIDAD_SALUD"];
+                    this.Traslado_Otra_Entidad = response.body.atributos[0]["TRASLADO_OTRA_ENTIDAD"];
+                    this.Fallecimiento = response.body.atributos[0]["FALLECIMIENTO"];
+                    this.RestitucionAseguramientoSaludo = response.body.atributos[0]["RESTITUCIONASEGURAMIENTOSALUDO"];
+                    this.Restitucion_Derechos_DNI = response.body.atributos[0]["RESTITUCION_DERECHOS_DNI"];
+                    this.RestitucionReinsercionFamiliar = response.body.atributos[0]["RESTITUCIONREINSERCIONFAMILIAR"];
+
+                }
+             });
+
+        },
+        
+    }
+  })
