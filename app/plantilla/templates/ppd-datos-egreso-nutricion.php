466992f37cf0b4c403f43ebb28bf0e9856a2f9b6
geral <geral_366@hotmail.com>
2018-12-29 12:48:59 -0500

    update

--- app/plantilla/templates/ppd-datos-egreso-nutricion.php
+++ app/plantilla/templates/ppd-datos-egreso-nutricion.php
@@ -101,17 +101,17 @@
                         <div class="row">
                             <div class="form-group col-md-4">
                                 <label for="text-input" class=" form-control-label">Peso (Kg.)</label>
-                                <input type="number" v-model="CarPeso" name="CarPeso" placeholder="" class="form-control">
+                                <input type="number" step=".02" v-model="CarPeso" name="CarPeso" placeholder="" class="form-control">
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="text-input" class=" form-control-label">Talla (m)</label>
-                                <input type="number" v-model="CarTalla" name="CarTalla" placeholder="" class="form-control">
+                                <input type="number" step=".02" v-model="CarTalla" name="CarTalla" placeholder="" class="form-control">
                             </div>
                         </div>
                         <div class="row">
                             <div class="form-group col-md-4">
                                 <label for="text-input" class=" form-control-label">Hemoglobina (gr./dl)</label>
-                                <input type="text" v-model="CarHemoglobina" name="CarHemoglobina" placeholder="" class="form-control">
+                                <input type="number" step=".02" v-model="CarHemoglobina" name="CarHemoglobina" placeholder="" class="form-control">
                             </div>
 
                         </div>
