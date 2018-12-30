9d9876b0d4dbd8c386aee36c8f63b1e1af855437
geralsantos <geral_366@hotmail.com>
2018-12-29 19:01:45 -0500

    update

---
+++ app/plantilla/templates/pam-datos-salud.php
@@ -0,0 +1,130 @@
+<template id="pam-datos-salud">
+    <div class="content mt-3">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Salud</strong>
+                    <h6>Formulario de Carga de Datos</h6>
+                </div>
+                <div class="card-body card-block">
+                    <form class="form-horizontal" v-on:submit.prevent="guardar">
+                    <div class="row">
+                            <div class="form-group col-md-7">
+                                <label for="text-input" class=" form-control-label">Nombre Residente</label>
+                                <div class="autocomplete">
+                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
+                                    <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
+                                        <li class="loading" v-if="isLoading">
+                                            Loading results...
+                                        </li>
+                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
+                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO}} - {{coincidencia.DOCUMENTO}}
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="text-input" class=" form-control-label">Año</label>
+                                <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                                <option value="2020">2020</option>
+                                <option value="2021">2021</option>
+                                </select>
+
+                            </div>
+                            <div class="form-group col-md-3">
+                                <div class=""><label for="text-input" class=" form-control-label">Mes</label>
+                                <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
+                                        <option value="1">Enero</option>
+                                        <option value="2">Febrero</option>
+                                        <option value="3">Marzo</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Mayo</option>
+                                        <option value="6">Junio</option>
+                                        <option value="7">Julio</option>
+                                        <option value="8">Agosto</option>
+                                        <option value="9">Septiembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Noviembre</option>
+                                        <option value="12">Diciembre</option>
+                                    </select> </div>
+                            </div>
+
+                        </div>
+                       
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Posee plan de intervención en Salud</label>
+                                <select name="Plan_Intervencion" v-model="Plan_Intervencion" class="form-control">
+                                <option value="Si">Si</option>
+                                <option value="No">No</option>
+                                </select> </div>
+                            </div>
+                            <div class="form-group col-md-8">
+                                <div class=" "><label for="text-input" class=" form-control-label">Descripción de la meta trazada en el pai</label>
+                                <input type="text" v-model="Meta_PAI" name="Meta_PAI" placeholder="" class="form-control">
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Posee informe técnico evolutivo</label>
+                                <select name="Informe_Tecnico" v-model="Informe_Tecnico" class="form-control">
+                                <option value="">Si</option>
+                                <option value="">No</option>
+                                </select> </div>
+                            </div>
+                            <div class="form-group col-md-8">
+                                <label for="text-input" class=" form-control-label">Descripción del informe evolutivo</label>
+                                <input type="text" v-model="Des_Informe_Tecnico" name="Des_Informe_Tecnico" placeholder="" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="text-input" class=" form-control-label">Cumplimiento del plan de intervención</label>
+                                <select name="Cumple_Intervencion" v-model="Cumple_Intervencion" class="form-control">
+                                <option value="Residente logra el objetivo trazado">Residente logra el objetivo trazado</option>
+                                <option value="En proceso">En proceso</option>
+                                <option value="Residente no logra el objetivo trazado">Residente no logra el objetivo trazado</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="text-input" class=" form-control-label">Grado dependencia de las PAM</label>
+                                <select name="Grado_PAM" v-model="Grado_PAM" class="form-control">
+                                <option value="Autovalente">Autovalente</option>
+                                <option value="Frágil">Frágil</option>
+                                <option value="Postrada o geríatrica compleja">Postrada o geríatrica compleja</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">Enfermedades crónicas degeneraritvas</label>
+                                <select name="EnfermedaCronicasDegenerativas" v-model="EnfermedaCronicasDegenerativas" class="form-control">
+                                <option value="Si">Si</option>
+                                <option value="No">No</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-8">
+                                <label for="text-input" class=" form-control-label">Especificar la enfermedad</label>
+                                <input type="text" v-model="Especificar_Enfermedad" name="Especificar_Enfermedad" placeholder="" class="form-control">
+                            </div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 text-center" >
+                                <button type="submit" class="btn btn-success btn-sm">
+                                    <i class="fa fa-send"></i> Grabar Datos
+                                </button>
+                            </div>
+                        </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div> <!-- .content -->
+</template>
