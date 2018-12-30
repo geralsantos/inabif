a3458f33dcc59f899c2d5b5c23d0bc23df1b110c
geralsantos <geral_366@hotmail.com>
2018-12-30 17:47:45 -0500

    update

--- app/plantilla/templates/nna-egreso-usuario.php
+++ app/plantilla/templates/nna-egreso-usuario.php
@@ -9,72 +9,117 @@
                 <div class="card-body card-block">
                     <form  class="form-horizontal" v-on:submit.prevent="guardar">
                         <div class="row">
+                            <div class="form-group col-md-7">
+                                <label for="text-input" class=" form-control-label">Nombre Residente</label>
+                                <div class="autocomplete">
+                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
+                                    <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
+                                        <li class="loading" v-if="isLoading">
+                                            Loading results...
+                                        </li>
+                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
+                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO}} - {{coincidencia.DOCUMENTO}}
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="text-input" class=" form-control-label">Año</label>
+                                <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                                <option value="2020">2020</option>
+                                <option value="2021">2021</option>
+                                </select>
+
+                            </div>
+                            <div class="form-group col-md-3">
+                                <div class=""><label for="text-input" class=" form-control-label">Mes</label>
+                                <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
+                                        <option value="1">Enero</option>
+                                        <option value="2">Febrero</option>
+                                        <option value="3">Marzo</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Mayo</option>
+                                        <option value="6">Junio</option>
+                                        <option value="7">Julio</option>
+                                        <option value="8">Agosto</option>
+                                        <option value="9">Septiembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Noviembre</option>
+                                        <option value="12">Diciembre</option>
+                                    </select> </div>
+                            </div>
+
+                        </div>
+                        <div class="row">
                             <div class="form-group col-md-4">
-                                <div class=" "><label for="text-input" class=" form-control-label">FECHA DE EGRESO</label>
-                                <input type="date" v-model="NNAFEgreso" name="NNAFEgreso" placeholder="" class="form-control">
+                                <div class=" "><label for="text-input" class=" form-control-label">Fecha de egreso</label>
+                                <input type="date" v-model="Fecha_Egreso" name="Fecha_Egreso" placeholder="" class="form-control">
                                     </div>
                             </div>
                             <div class="form-group col-md-4">
-                                <div class=" "><label for="text-input" class=" form-control-label">MOTIVO DEL EGRESO</label>
-                                <select name="NNAMotivoEgreso" v-model="NNAMotivoEgreso" class="form-control">
-                                <option value="">Acogimiento Familiar</option>
-                                <option value="">Adopción</option>
-                                <option value="">Defunción</option>
-                                <option value="">Egreso por mayoría de edad</option>
-                                <option value="">Reinserción familiar</option>
-                                <option value="">Salida no autorizada</option>
-                                <option value="">Traslado a otra institución</option>
-                                <option value="">Traslado a otro CAR del INABIF</option>
-                                <option value=""> Otros</option>
+                                <div class=" "><label for="text-input" class=" form-control-label">Motivo de egreso</label>
+                                <select name="MotivoEgreso" v-model="MotivoEgreso" class="form-control">
+                                <option value="Acogimiento familiar">Acogimiento familiar</option>
+                                <option value="Adopción">Adopción</option>
+                                <option value="Defunción">Defunción</option>
+                                <option value="Egreso por mayoría de edad">Egreso por mayoría de edad</option>
+                                <option value="Reinserción familiar">Reinserción familiar</option>
+                                <option value="Salida no autorizada">Salida no autorizada</option>
+                                <option value="Traslado a otra institución">Traslado a otra institución</option>
+                                <option value="Traslado a otro CAR del INABIF">Traslado a otro CAR del INABIF</option>
+                                <option value="Otros">Otros</option>
                                 </select> </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Detalle del motivo del egreso</label>
-                                <input type="text" v-model="NNADetalleMotivoEgreso" name="NNADetalleMotivoEgreso" placeholder="" class="form-control"> </div>
+                                <input type="text" v-model="Detalle_Motivo" name="Detalle_Motivo" placeholder="" class="form-control"> </div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="form-group col-md-4">
-                                <label for="text-input" class=" form-control-label">Aseguramiento Universal de Salud-AUS</label>
-                                    <select name="NNASaludAUS" v-model="NNASaludAUS" class="form-control">
-                                    <option value="">Si</option>
-                                    <option value="">No</option>
+                                <label for="text-input" class=" form-control-label">Aseguramiento universal de Salud-AUS</label>
+                                    <select name="Salud_AUS" v-model="Salud_AUS" class="form-control">
+                                        <option value="Si">Si</option>
+                                        <option value="No">No</option>
                                     </select>
                             </div>
                             <div class="form-group col-md-4">
-                                <label for="text-input" class=" form-control-label">Partida de Nacimiento?</label>
-                                    <select name="NNAPartidaNacimiento" v-model="NNAPartidaNacimiento" class="form-control">
-                                    <option value="">Si</option>
-                                    <option value="">No</option>
+                                <label for="text-input" class=" form-control-label">¿Partida de Nacimiento?</label>
+                                    <select name="Partida_Naci" v-model="Partida_Naci" class="form-control">
+                                    <option value="Si">Si</option>
+                                        <option value="No">No</option>
                                     </select>
                             </div>
                             <div class="form-group col-md-4">
-                                <label for="text-input" class=" form-control-label">DNI?</label>
-                                    <select name="NNATieneDNI" v-model="NNATieneDNI" class="form-control">
-                                    <option value="">Si</option>
-                                    <option value="">No</option>
+                                <label for="text-input" class=" form-control-label">¿DNI?</label>
+                                    <select name="DNI" v-model="DNI" class="form-control">
+                                    <option value="Si">Si</option>
+                                        <option value="No">No</option>
                                     </select>
                             </div>
                         </div>
                         <div class="row">
 
                             <div class="form-group col-md-4">
-                                <label for="text-input" class=" form-control-label">Educación?</label>
-                                <select name="NNAEducacion" v-model="NNAEducacion" class="form-control">
-                                    <option value="">Si</option>
-                                    <option value="">No</option>
-                                    </select>
+                                <label for="text-input" class=" form-control-label">¿Educación?</label>
+                                <select name="Educacion" v-model="Educacion" class="form-control">
+                                    <option value="Si">Si</option>
+                                        <option value="No">No</option>
+                                </select>
                             </div>
                             <div class="form-group col-md-4">
                                 <label for="text-input" class=" form-control-label">Reinserción Familiar</label>
-                                    <select name="NNAReinsercionFamiliar" v-model="NNAReinsercionFamiliar" class="form-control">
-                                    <<option value="">Padre</option>
-                                    <option value="">Madre</option>
-                                    <<option value="">Hermano</option>
-                                    <option value="">Tío/a</option>
-                                    <option value="">primos/as</option>
-                                    <option value=""> Abuelos/as</option>
-                                    <option value="">Otros</option>
+                                    <select name="Reinsecion_Familiar" v-model="Reinsecion_Familiar" class="form-control">
+                                    <<option value="Padre">Padre</option>
+                                    <option value="Madre">Madre</option>
+                                    <<option value="Hermano">Hermano</option>
+                                    <option value="Tío/a">Tío/a</option>
+                                    <option value="Primos/as">Primos/as</option>
+                                    <option value="Abuelos/as">Abuelos/as</option>
+                                    <option value="Otros">Otros</option>
                                     </select>
                             </div>
                         </div>
--- js/componentes/portada/nna-egreso-usuario.js
+++ js/componentes/portada/nna-egreso-usuario.js
@@ -1,14 +1,23 @@
 Vue.component('nna-egreso-usuario', {
     template: '#nna-egreso-usuario',
     data:()=>({
-        NNAFEgreso:null,
-        NNAMotivoEgreso:null,
-        NNADetalleMotivoEgreso:null,
-        NNASaludAUS:null,
-        NNAPartidaNacimiento:null,
-        NNATieneDNI:null,
-        NNAEducacion:null,
-        NNAReinsercionFamiliar:null
+     
+        Fecha_Egreso:null,
+        MotivoEgreso:null,
+        Detalle_Motivo:null,
+        Salud_AUS :null,
+        Partida_Naci :null,
+        DNI :null,
+        Educacion :null,
+        Reinsecion_Familiar:null,
+
+        nombre_residente:null,
+        isLoading:false,
+        mes:moment().format("MM"),
+        anio:(new Date()).getFullYear(),
+        coincidencias:[],
+        bloque_busqueda:false,
+        id_residente:null
 
     }),
     created:function(){
@@ -19,18 +28,28 @@ Vue.component('nna-egreso-usuario', {
     },
     methods:{
         guardar(){
+            if (this.id_residente==null) {
+                swal('Error', 'Residente no existe', 'warning');
+                return false;
+            }
             let valores = {
-                Fecha_Egreso:this.NNAFEgreso,
-                MotivoEgreso:this.NNAMotivoEgreso,
-                Detalle_Motivo:this.NNADetalleMotivoEgreso,
-                Salud_AUS:this.NNASaludAUS,
-                Partida_Naci:this.NNAPartidaNacimiento,
-                DNI:this.NNATieneDNI,
-                Educacion:this.NNAEducacion,
-                Reinsecion_Familiar:this.NNAReinsercionFamiliar
+               
+                Fecha_Egreso:this.Fecha_Egreso,
+                MotivoEgreso:this.MotivoEgreso,
+                Detalle_Motivo:this.Detalle_Motivo,
+                Salud_AUS :this.Salud_AUS,
+                Partida_Naci :this.Partida_Naci,
+                DNI :this.DNI,
+                Educacion :this.Educacion,
+                Reinsecion_Familiar:this.Reinsecion_Familiar,
 
-                        }
-            this.$http.post('insertar_datos?view',{tabla:'EgresoUsuario', valores:valores}).then(function(response){
+                Residente_Id: this.id_residente,
+                Periodo_Mes: moment().format("MM"),
+                Periodo_Anio:moment().format("YYYY")
+
+            }
+                
+            this.$http.post('insertar_datos?view',{tabla:'NNAEgresoUsuario', valores:valores}).then(function(response){
 
                 if( response.body.resultado ){
                     swal('', 'Registro Guardado', 'success');
@@ -39,6 +58,56 @@ Vue.component('nna-egreso-usuario', {
                   swal("", "Un error ha ocurrido", "error");
                 }
             });
-        }
+        },
+        buscar_residente(){
+            this.id_residente = null;
+
+            var word = this.nombre_residente;
+            if( word.length >= 4){
+                this.coincidencias = [];
+                this.bloque_busqueda = true;
+                this.isLoading = true;
+
+                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
+
+                    if( response.body.data != undefined){
+                        this.isLoading = false;
+                        this.coincidencias = response.body.data;
+                    }else{
+                        this.bloque_busqueda = false;
+                        this.isLoading = false;
+                        this.coincidencias = [];
+                    }
+                 });
+            }else{
+                this.bloque_busqueda = false;
+                this.isLoading = false;
+                this.coincidencias = [];
+            }
+        },
+        actualizar(coincidencia){
+            this.id_residente = coincidencia.ID;
+            this.nombre_residente=coincidencia.NOMBRE;
+            this.coincidencias = [];
+            this.bloque_busqueda = false;
+
+            this.$http.post('cargar_datos_residente?view',{tabla:'NNAEgresoUsuario', residente_id:this.id_residente }).then(function(response){
+
+                if( response.body.atributos != undefined){
+
+                    this.Fecha_Egreso = response.body.atributos[0]["FECHA_EGRESO"];
+                    this.MotivoEgreso = response.body.atributos[0]["MOTIVOEGRESO"];
+                    this.Detalle_Motivo = response.body.atributos[0]["DETALLE_MOTIVO"];
+                    this.Salud_AUS = response.body.atributos[0]["SALUD_AUS"];
+                    this.Partida_Naci = response.body.atributos[0]["PARTIDA_NACI"];
+                    this.DNI = response.body.atributos[0]["DNI"];
+                    this.Educacion = response.body.atributos[0]["EDUCACION"];
+                    this.Reinsecion_Familiar = response.body.atributos[0]["REINSECION_FAMILIAR"];
+                 
+                }
+             });
+
+        },
+        
     }
   })
