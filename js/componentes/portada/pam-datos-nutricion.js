6a782947d1e40b0de107212f5da895f4160211f4
geral <geral_366@hotmail.com>
2018-12-30 14:57:59 -0500

    update

--- js/componentes/portada/pam-datos-nutricion.js
+++ js/componentes/portada/pam-datos-nutricion.js
@@ -1,101 +1,101 @@
 Vue.component('pam-datos-nutricion', {
-    template:'#pam-datos-nutricion',
-    data:()=>({
-        Plan_Intervencion:null,
-        Meta_PAI:null,
-        Informe_Tecnico:null,
-        Des_Informe_Tecnico:null,
-        Cumple_Intervencion:null,
-        Estado_Nutricional_IMC:null,
-        Peso:null,
-        Talla:null,
-        Hemoglobina:null,
-             
-        nombre_residente:null,
-        isLoading:false,
-        mes:moment().format("MM"),
-        anio:(new Date()).getFullYear(),
-        coincidencias:[],
-        bloque_busqueda:false,
-        id_residente:null
+    template: '#pam-datos-nutricion',
+    data: () => ({
+        Plan_Intervencion: null,
+        Meta_PAI: null,
+        Informe_Tecnico: null,
+        Des_Informe_Tecnico: null,
+        Cumple_Intervencion: null,
+        Estado_Nutricional_IMC: null,
+        Peso: null,
+        Talla: null,
+        Hemoglobina: null,
+
+        nombre_residente: null,
+        isLoading: false,
+        mes: moment().format("MM"),
+        anio: (new Date()).getFullYear(),
+        coincidencias: [],
+        bloque_busqueda: false,
+        id_residente: null
 
 
     }),
-    created:function(){
+    created: function () {
     },
-    mounted:function(){
-      
+    mounted: function () {
+
     },
-    updated:function(){
+    updated: function () {
     },
-    methods:{
-        guardar(){
-            if (this.id_residente==null) {
+    methods: {
+        guardar() {
+            if (this.id_residente == null) {
                 swal('Error', 'Residente no existe', 'success');
                 return false;
             }
             let valores = {
-                
-                Plan_Intervencion:this.Plan_Intervencion,
-                Meta_PAI:this.Meta_PAI,
-                Informe_Tecnico:this.Informe_Tecnico,
-                Des_Informe_Tecnico:this.Des_Informe_Tecnico,
-                Cumple_Intervencion:this.Cumple_Intervencion,
-                Estado_Nutricional_IMC:this.Estado_Nutricional_IMC,
-                Peso:this.Peso,
-                Talla:this.Talla,
-                Hemoglobina:this.Hemoglobina,
+
+                Plan_Intervencion: this.Plan_Intervencion,
+                Meta_PAI: this.Meta_PAI,
+                Informe_Tecnico: this.Informe_Tecnico,
+                Des_Informe_Tecnico: this.Des_Informe_Tecnico,
+                Cumple_Intervencion: this.Cumple_Intervencion,
+                Estado_Nutricional_IMC: this.Estado_Nutricional_IMC,
+                Peso: this.Peso,
+                Talla: this.Talla,
+                Hemoglobina: this.Hemoglobina,
                 Residente_Id: this.id_residente,
                 Periodo_Mes: moment().format("MM"),
-                Periodo_Anio:moment().format("YYYY")
-    
-            }
-             
-            this.$http.post('insertar_datos?view',{tabla:'pam_nutricion', valores:valores}).then(function(response){
+                Periodo_Anio: moment().format("YYYY")
 
-                if( response.body.resultado ){
+            }
+
+            this.$http.post('insertar_datos?view', { tabla: 'pam_nutricion', valores: valores }).then(function (response) {
+
+                if (response.body.resultado) {
                     swal('', 'Registro Guardado', 'success');
 
-                }else{
-                  swal("", "Un error ha ocurrido", "error");
+                } else {
+                    swal("", "Un error ha ocurrido", "error");
                 }
             });
         },
-        buscar_residente(){
+        buscar_residente() {
             this.id_residente = null;
 
             var word = this.nombre_residente;
-            if( word.length >= 4){
+            if (word.length >= 4) {
                 this.coincidencias = [];
                 this.bloque_busqueda = true;
                 this.isLoading = true;
 
-                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
+                this.$http.post('ejecutar_consulta?view', { like: word }).then(function (response) {
 
-                    if( response.body.data != undefined){
+                    if (response.body.data != undefined) {
                         this.isLoading = false;
                         this.coincidencias = response.body.data;
-                    }else{
+                    } else {
                         this.bloque_busqueda = false;
                         this.isLoading = false;
                         this.coincidencias = [];
                     }
-                 });
-            }else{
+                });
+            } else {
                 this.bloque_busqueda = false;
                 this.isLoading = false;
                 this.coincidencias = [];
             }
         },
-        actualizar(coincidencia){
+        actualizar(coincidencia) {
             this.id_residente = coincidencia.ID;
-            this.nombre_residente=coincidencia.NOMBRE;
+            this.nombre_residente = coincidencia.NOMBRE;
             this.coincidencias = [];
             this.bloque_busqueda = false;
 
-            this.$http.post('cargar_datos_residente?view',{tabla:'pam_nutricion', residente_id:this.id_residente }).then(function(response){
+            this.$http.post('cargar_datos_residente?view', { tabla: 'pam_nutricion', residente_id: this.id_residente }).then(function (response) {
 
-                if( response.body.atributos != undefined){
+                if (response.body.atributos != undefined) {
 
                     this.Plan_Intervencion = response.body.atributos[0]["PLAN_INTERVENCION"];
                     this.Meta_PAI = response.body.atributos[0]["META_PAI"];
@@ -106,11 +106,11 @@ Vue.component('pam-datos-nutricion', {
                     this.Peso = response.body.atributos[0]["PESO"];
                     this.Talla = response.body.atributos[0]["TALLA"];
                     this.Hemoglobina = response.body.atributos[0]["HEMOGLOBINA"];
-                    
+
                 }
-             });
+            });
 
         },
-       
+
     }
-  })
+})
