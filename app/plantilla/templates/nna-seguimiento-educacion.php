e1ff7118338164e54a2435b1621950330ee4821e
geralsantos <geral_366@hotmail.com>
2018-12-30 18:13:36 -0500

    update

---
+++ app/plantilla/templates/nna-seguimiento-educacion.php
@@ -0,0 +1,152 @@
+<template id="nna-seguimiento-educacion">
+    <div class="content mt-3">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Seguimiento - Educación</strong>
+                    <h6>Formulario de Carga de Datos</h6>
+                </div>
+                <div class="card-body card-block">
+                    <form  class="form-horizontal" v-on:submit.prevent="guardar">
+                        <div class="row">
+                            <div class="form-group col-md-7">
+                                <label for="text-input" class=" form-control-label">Nombre Residente</label>
+                                <div class="autocomplete">
+                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
+                                    <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
+                                        <li class="loading" v-if="isLoading">
+                                            Loading results...
+                                        </li>
+                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
+                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO}} - {{coincidencia.DOCUMENTO}}
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="text-input" class=" form-control-label">Año</label>
+                                <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                                <option value="2020">2020</option>
+                                <option value="2021">2021</option>
+                                </select>
+
+                            </div>
+                            <div class="form-group col-md-3">
+                                <div class=""><label for="text-input" class=" form-control-label">Mes</label>
+                                <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
+                                        <option value="1">Enero</option>
+                                        <option value="2">Febrero</option>
+                                        <option value="3">Marzo</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Mayo</option>
+                                        <option value="6">Junio</option>
+                                        <option value="7">Julio</option>
+                                        <option value="8">Agosto</option>
+                                        <option value="9">Septiembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Noviembre</option>
+                                        <option value="12">Diciembre</option>
+                                    </select> </div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Plan de Intervención Educativo?</label>
+                                <select name="Plan_Intervencion" v-model="Plan_Intervencion" class="form-control">
+                                    <option value="Si">Si</option>
+                                    <option value="No">No</option>
+                                </select> </div>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Matriculado en el sistema educativo</label>
+                                <select name="Sistema_Educativo" v-model="Sistema_Educativo" class="form-control">
+                                    <option value="Regular">Regular</option>
+                                    <option value="CEBA">CEBA</option>
+                                    <option value="CEBE">CEBE</option>
+                                    <option value="CEPTRO">CEPTRO</option>
+                                </select> </div>
+                                
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Nivel que cursa?</label>
+                                <select name="NEducativo" v-model="NEducativo" class="form-control">
+                                <option value="Inicial">Inicial</option>
+                                <option value="Primaria">Primaria</option>
+                                <option value="Secundaria">Secundaria</option>
+                                <option value="Superior Universitario">Superior Universitario</option>
+                                <option value="Superior Tecnológica">Superior Tecnológica</option>
+                                <option value="Superior Artística">Superior Artística</option>
+                                <option value="Superior Pedagógica">Superior Pedagógica</option>
+                                </select>  </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">Grado </label>
+                                    <select name="Grado" v-model="Grado" class="form-control">
+                                        <option value="Primaria: 1 a 6 to grado">Primaria: 1 a 6 to grado</option>
+                                        <option value="Secundaria: 1 a 5to grado">Secundaria: 1 a 5to grado</option>
+                                    </select> 
+                                    
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">Acude en el mes al centro de estudios?</label>
+                                    <select name="Asitencia" v-model="Asitencia" class="form-control">
+                                    <option value="Si">Si</option>
+                                    <option value="No">No</option>
+                                    </select> 
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">N° de días de asistencia</label>
+                                <input type="number"  min="0" v-model="Nro_Asistencia" name="Nro_Asistencia" value='5' placeholder="" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row">
+                            
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">N° de sesiones de reforzamiento escolar</label>
+                                <input type="number"  min="0" v-model="Nro_Reforzamientos" name="Nro_Reforzamientos" value='5' placeholder="" class="form-control">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">Número de Aprestamiento Escolar</label>
+                                <input type="number"  min="0" v-model="Nro_Aprestamiento" name="Nro_Aprestamiento" value='5' placeholder="" class="form-control">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">N° de consejerías y orientaciones - Área educativa</label>
+                                <input type="number"  min="0" v-model="Nro_Consejera" name="Nro_Consejera" value='5' placeholder="" class="form-control">
+                            </div>
+                                                        
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">Se encuentran participando de entrenamiento profesional/técnico</label>
+                                    <select name="Estado_Participacion" v-model="Estado_Participacion" class="form-control">
+                                    <option value="Inserciones Laborales">Inserciones Laborales </option>
+                                    <option value="Prácticas profesionales o técnicas">Prácticas profesionales o técnicas</option>
+                                    <option value="Programa de entrenamiento no remunerado">Programa de entrenamiento no remunerado</option>
+                                    </select> 
+                                    
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">Especificar la actividad / oficio</label>
+                                    <textarea name="ActividadOficio" v-model="ActividadOficio" col="30" ros="2"></textarea>
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <div class="col-md-12 text-center" >
+                                <button type="submit" class="btn btn-success btn-sm">
+                                    <i class="fa fa-send"></i> Grabar Datos
+                                </button>
+                            </div>
+                        </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div> <!-- .content -->
+</template>
---
+++ js/componentes/portada/nna-seguimiento-educacion.js
@@ -0,0 +1,120 @@
+Vue.component('nna-seguimiento-educacion', {
+    template: '#nna-seguimiento-educacion',
+    data:()=>({
+     
+        Plan_Intervencion:null,
+        Sistema_Educativo:null,
+        NEducativo:null,
+        Grado :null,
+        Asitencia :null,
+        Nro_Asistencia :null,
+        Nro_Reforzamientos :null,
+        Nro_Aprestamiento:null,
+        Nro_Consejera :null,
+        Estado_Participacion :null,
+        ActividadOficio:null,
+
+        nombre_residente:null,
+        isLoading:false,
+        mes:moment().format("MM"),
+        anio:(new Date()).getFullYear(),
+        coincidencias:[],
+        bloque_busqueda:false,
+        id_residente:null
+
+    }),
+    created:function(){
+    },
+    mounted:function(){
+    },
+    updated:function(){
+    },
+    methods:{
+        guardar(){
+            if (this.id_residente==null) {
+                swal('Error', 'Residente no existe', 'warning');
+                return false;
+            }
+            let valores = {
+               
+                Plan_Intervencion:this.Plan_Intervencion,
+                Sistema_Educativo:this.Sistema_Educativo,
+                NEducativo:this.NEducativo,
+                Grado :this.Grado,
+                Asitencia :this.Asitencia,
+                Nro_Asistencia :this.Nro_Asistencia,
+                Nro_Reforzamientos :this.Nro_Reforzamientos,
+                Nro_Aprestamiento:this.Nro_Aprestamiento,
+                Nro_Consejera :this.Nro_Consejera,
+                Estado_Participacion :this.Estado_Participacion,
+                ActividadOficio:this.ActividadOficio,
+
+                Residente_Id: this.id_residente,
+                Periodo_Mes: moment().format("MM"),
+                Periodo_Anio:moment().format("YYYY")
+
+            }
+                
+            this.$http.post('insertar_datos?view',{tabla:'NNAEducacion', valores:valores}).then(function(response){
+
+                if( response.body.resultado ){
+                    swal('', 'Registro Guardado', 'success');
+
+                }else{
+                  swal("", "Un error ha ocurrido", "error");
+                }
+            });
+        },
+        buscar_residente(){
+            this.id_residente = null;
+
+            var word = this.nombre_residente;
+            if( word.length >= 4){
+                this.coincidencias = [];
+                this.bloque_busqueda = true;
+                this.isLoading = true;
+
+                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
+
+                    if( response.body.data != undefined){
+                        this.isLoading = false;
+                        this.coincidencias = response.body.data;
+                    }else{
+                        this.bloque_busqueda = false;
+                        this.isLoading = false;
+                        this.coincidencias = [];
+                    }
+                 });
+            }else{
+                this.bloque_busqueda = false;
+                this.isLoading = false;
+                this.coincidencias = [];
+            }
+        },
+        actualizar(coincidencia){
+            this.id_residente = coincidencia.ID;
+            this.nombre_residente=coincidencia.NOMBRE;
+            this.coincidencias = [];
+            this.bloque_busqueda = false;
+
+            this.$http.post('cargar_datos_residente?view',{tabla:'NNAEducacion', residente_id:this.id_residente }).then(function(response){
+
+                if( response.body.atributos != undefined){
+
+                    this.Plan_Intervencion = response.body.atributos[0]["PLAN_INTERVENCION"];
+                    this.Sistema_Educativo = response.body.atributos[0]["SISTEMA_EDUCATIVO"];
+                    this.NEducativo = response.body.atributos[0]["NEDUCATIVO"];
+                    this.Grado = response.body.atributos[0]["GRADO"];
+                    this.Asitencia = response.body.atributos[0]["ASITENCIA"];
+                    this.Nro_Reforzamientos = response.body.atributos[0]["NRO_REFORZAMIENTOS"];
+                    this.Nro_Aprestamiento = response.body.atributos[0]["NRO_APRESTAMIENTO"];
+                    this.Nro_Consejera = response.body.atributos[0]["NRO_CONSEJERA"];
+                    this.Estado_Participacion = response.body.atributos[0]["ESTADO_PARTICIPACION"];
+                    this.ActividadOficio = response.body.atributos[0]["ACTIVIDADOFICIO"];
+                }
+             });
+
+        },
+        
+    }
+  })
