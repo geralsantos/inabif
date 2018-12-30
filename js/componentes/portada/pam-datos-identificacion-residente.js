7efd176c4c3c61b1ed57f54720416eaed5d74b69
geral <geral_366@hotmail.com>
2018-12-30 14:25:04 -0500

    update

--- js/componentes/portada/pam-datos-identificacion-residente.js
+++ js/componentes/portada/pam-datos-identificacion-residente.js
@@ -127,7 +127,7 @@ console.log(valores);
                     this.provincia_nacimiento_id = response.body.atributos[0]["PROVINCIA_NACIMIENTO_ID"];
                     this.distrito_nacimiento_id = response.body.atributos[0]["DISTRITO_NACIMIENTO_ID"];
                     this.Sexo = response.body.atributos[0]["SEXO"];
-                    this.Fecha_Nacimiento = response.body.atributos[0]["FECHA_NACIMIENTO"];
+                    this.Fecha_Nacimiento = moment(response.body.atributos[0]["FECHA_NACIMIENTO"]).format("YYYY-MM-DD");
                     this.Edad = response.body.atributos[0]["EDAD"];
                     this.Lengua_Materna = response.body.atributos[0]["LENGUA_MATERNA"];
                 }
