b91b27665fc0b7226c656475946309dc83481306
geralsantos <geral_366@hotmail.com>
2018-12-30 20:45:48 -0500

    update

---
+++ app/plantilla/templates/nna-seguimientos-trabajo-social.php
@@ -0,0 +1,123 @@
+<template id="nna-seguimientos-trabajo-social">
+    <div class="content mt-3">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Seguimiento Trabajo Social</strong>
+                    <h6>Formulario de Carga de Datos</h6>
+                </div>
+                <div class="card-body card-block">
+                    <form  class="form-horizontal" v-on:submit.prevent="guardar">
+                        <div class="row">
+                            <div class="form-group col-md-7">
+                                <label for="text-input" class=" form-control-label">Nombre Residente</label>
+                                <div class="autocomplete">
+                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
+                                    <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
+                                        <li class="loading" v-if="isLoading">
+                                            Loading results...
+                                        </li>
+                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
+                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO}} - {{coincidencia.DOCUMENTO}}
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="text-input" class=" form-control-label">Año</label>
+                                <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                                <option value="2020">2020</option>
+                                <option value="2021">2021</option>
+                                </select>
+
+                            </div>
+                            <div class="form-group col-md-3">
+                                <div class=""><label for="text-input" class=" form-control-label">Mes</label>
+                                <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
+                                        <option value="1">Enero</option>
+                                        <option value="2">Febrero</option>
+                                        <option value="3">Marzo</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Mayo</option>
+                                        <option value="6">Junio</option>
+                                        <option value="7">Julio</option>
+                                        <option value="8">Agosto</option>
+                                        <option value="9">Septiembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Noviembre</option>
+                                        <option value="12">Diciembre</option>
+                                    </select> </div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Plan de Intervención </label>
+                                <select name="Plan_Intervencion" id="Plan_Intervencion" class="form-control">
+                                <option value="Si">Si</option>
+                                <option value="No">No</option>
+                                </select> </div>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Meta trazada en el PAI</label>
+                                <input type="text" id="Meta_PAI" name="Meta_PAI" value='Meta' placeholder="" class="form-control">  </div>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class=" "><label for="text-input" class=" form-control-label">Posee informe técnico evolutivo?</label>
+                                <select name="Informe_Tecnico" id="Informe_Tecnico" class="form-control">
+                                <option value="Si">Si</option>
+                                <option value="No">No</option>
+                                </select>  </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">¿Cumplimiento del Plan de intervención</label>
+                                    <select name="Cumple_Intervencion" id="Cumple_Intervencion" class="form-control">
+                                    <option value="Cumplida">Cumplida</option>
+                                    <option value="En proceso">En proceso</option>
+                                    <option value="No se evidencia progreso">No se evidencia progreso</option>
+                                    </select>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">¿Participación activa familiar?</label>
+                                    <select name="ParticipacionF_Activa" id="ParticipacionF_Activa" class="form-control">
+                                    <option value="Si">Si</option>
+                                    <option value="No">No</option>
+                                    </select>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">¿Plan de Reinserción familiar?</label>
+                                    <select name="Reinsercion_Familiar" id="Reinsercion_Familiar" class="form-control">
+                                    <option value="Si">Si</option>
+                                <option value="No">No</option>
+                                    </select>
+                            </div>
+                        </div>
+                        <div class="row">
+
+                            <div class="form-group col-md-4">
+                                <label for="text-input" class=" form-control-label">Familia usa las redes de soporte social?</label>
+                                <select name="FamiliaR_Soporte" id="FamiliaR_Soporte" class="form-control">
+                                <option value="Si">Si</option>
+                                <option value="No">No</option>
+                                    </select>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 text-center" >
+                                <button type="submit" class="btn btn-success btn-sm">
+                                    <i class="fa fa-send"></i> Grabar Datos
+                                </button>
+                            </div>
+                        </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div> <!-- .content -->
+</template>
---
+++ js/componentes/portada/nna-seguimientos-trabajo-social.js
@@ -0,0 +1,110 @@
+Vue.component('nna-seguimientos-trabajo-social', {
+    template: '#nna-seguimientos-trabajo-social',
+    data:()=>({
+     
+        Plan_Intervencion:null,
+        Meta_PAI :null,
+        Informe_Tecnico :null,
+        Cumple_Intervencion:null,
+        ParticipacionF_Activa:null,
+        Reinsercion_Familiar:null,
+        FamiliaR_Soporte:null,
+                        
+        nombre_residente:null,
+        isLoading:false,
+        mes:moment().format("MM"),
+        anio:(new Date()).getFullYear(),
+        coincidencias:[],
+        bloque_busqueda:false,
+        id_residente:null
+
+    }),
+    created:function(){
+    },
+    mounted:function(){
+    },
+    updated:function(){
+    },
+    methods:{
+        guardar(){
+            if (this.id_residente==null) {
+                swal('Error', 'Residente no existe', 'warning');
+                return false;
+            }
+            let valores = {
+               
+                Plan_Intervencion:this,Plan_Intervencion,
+                Meta_PAI :this.Meta_PAI,
+                Informe_Tecnico :this.Informe_Tecnico,
+                Cumple_Intervencion:this.Cumple_Intervencion,
+                ParticipacionF_Activa:this.ParticipacionF_Activa,
+                Reinsercion_Familiar:this.Reinsercion_Familiar,
+                FamiliaR_Soporte:this.FamiliaR_Soporte,
+
+                Residente_Id: this.id_residente,
+                Periodo_Mes: moment().format("MM"),
+                Periodo_Anio:moment().format("YYYY")
+
+            }
+                
+            this.$http.post('insertar_datos?view',{tabla:'NNAtrabajoSocial_Semestral', valores:valores}).then(function(response){
+
+                if( response.body.resultado ){
+                    swal('', 'Registro Guardado', 'success');
+
+                }else{
+                  swal("", "Un error ha ocurrido", "error");
+                }
+            });
+        },
+        buscar_residente(){
+            this.id_residente = null;
+
+            var word = this.nombre_residente;
+            if( word.length >= 4){
+                this.coincidencias = [];
+                this.bloque_busqueda = true;
+                this.isLoading = true;
+
+                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
+
+                    if( response.body.data != undefined){
+                        this.isLoading = false;
+                        this.coincidencias = response.body.data;
+                    }else{
+                        this.bloque_busqueda = false;
+                        this.isLoading = false;
+                        this.coincidencias = [];
+                    }
+                 });
+            }else{
+                this.bloque_busqueda = false;
+                this.isLoading = false;
+                this.coincidencias = [];
+            }
+        },
+        actualizar(coincidencia){
+            this.id_residente = coincidencia.ID;
+            this.nombre_residente=coincidencia.NOMBRE;
+            this.coincidencias = [];
+            this.bloque_busqueda = false;
+
+            this.$http.post('cargar_datos_residente?view',{tabla:'NNAtrabajoSocial_Semestral', residente_id:this.id_residente }).then(function(response){
+
+                if( response.body.atributos != undefined){
+
+                    this.Plan_Intervencion = response.body.atributos[0]["PLAN_INTERVENCION"];
+                    this.Meta_PAI = response.body.atributos[0]["META_PAI"];
+                    this.Informe_Tecnico = response.body.atributos[0]["INFORME_TECNICO"];
+                    this.Cumple_Intervencion = response.body.atributos[0]["CUMPLE_INTERVENCION"];
+                    this.ParticipacionF_Activa = response.body.atributos[0]["PARTICIPACIONF_ACTIVA"];
+                    this.Reinsercion_Familiar = response.body.atributos[0]["REINSERCION_FAMILIAR"];
+                    this.FamiliaR_Soporte = response.body.atributos[0]["FAMILIAR_SOPORTE"];                  
+
+                }
+             })
+
+        },
+        
+    }
+  })
