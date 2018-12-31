54eb3f74950a90836d588a18c0ee794f17841f9a
geral <geral_366@hotmail.com>
2018-12-31 11:25:46 -0500

    update

--- app/plantilla/templates/registro-perfiles.php
+++ app/plantilla/templates/registro-perfiles.php
@@ -1,4 +1,68 @@
 <template id="registro-perfiles">
+    <style>
+    .modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}</style>
     <div class="content mt-3">
         <div class="col-lg-12">
             <div class="card">
@@ -7,7 +71,7 @@
                     <h6>Formulario de Carga de Datos</h6>
                 </div>
                 <div class="card-body card-block">
-                    <button class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Crear Usuario</button><br>
+                    <button class="btn btn-success" @click="showModal = false" >Crear Usuario</button><br>
                 <table id="bootstrap-data-table" class="table table-striped table-bordered text-center">
                     <thead>
                     <tr>
@@ -32,18 +96,21 @@
             </div>
         </div>
     </div> <!-- .content -->
-    <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form  class="form-horizontal"  v-on:submit.prevent="guardar">
+    <div v-if="showModal">
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Usuario</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" @click="showModal = false">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <form  class="form-horizontal"  v-on:submit.prevent="guardar">
                         <div class="row">
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Apellidos</label>
@@ -92,13 +159,15 @@
                             </div>
                         </div>
                         </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" @click="showModal = false">Cerrar</button>
+                </div>
+            </div>
+        </div>
+
+        </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-secondary" @click="showModal = false">Cerrar</button>
-      </div>
-    </div>
-</div>    
-    
+    </transition>
   </div>
 </template>
