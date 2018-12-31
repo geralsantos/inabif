1919350fba0e7fdba0309912267c1da12babad99
geral <geral_366@hotmail.com>
2018-12-31 11:49:10 -0500

    update

--- app/modulo/acceso/modelo.acceso.php
+++ app/modulo/acceso/modelo.acceso.php
@@ -4,9 +4,9 @@ class modeloAcceso extends MySQL{
     public function getSesion($usuario, $clave){
         try {
             $usuario = $this->executeQuery("select us.*,us.id as id_usuario,ca.centro_id as id_centro from usuarios us left join centro_atencion ca on(ca.ID=us.centro_id) where us.usuario=:usuario and us.clave=:clave",array("usuario"=>$usuario,"clave"=>$clave));
-            return $usuario;
-        } catch (\Throwable $th) {
+        } catch (PDOException $e) {
             return false;
         }
+        return $usuario;
     }
 }
