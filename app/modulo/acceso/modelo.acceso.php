5a7598ed3904c566e84e1363b4a0f5363e292604
geral <geral_366@hotmail.com>
2018-12-31 18:51:41 -0500

    update

--- app/modulo/acceso/modelo.acceso.php
+++ app/modulo/acceso/modelo.acceso.php
@@ -3,7 +3,7 @@ class modeloAcceso extends MySQL{
 
     public function getSesion($usuario, $clave){
         try {
-            $usuario = $this->executeQuery("select us.*,us.id as id_usuario,ca.centro_id as id_centro,ca.tipo_centro_id   from usuarios us left join centro_atencion ca on(ca.ID=us.centro_id) left join tipo_centro tc on (tc.id=ca.tipo_centro_id)  where us.usuario=:usuario and us.clave=:clave",array("usuario"=>$usuario,"clave"=>$clave));
+            $usuario = $this->executeQuery("select us.*,us.id as id_usuario,ca.id as id_centro,ca.tipo_centro_id   from usuarios us left join centro_atencion ca on(ca.ID=us.centro_id) left join tipo_centro tc on (tc.id=ca.tipo_centro_id)  where us.usuario=:usuario and us.clave=:clave",array("usuario"=>$usuario,"clave"=>$clave));
         } catch (PDOException $e) {
             return false;
         }
