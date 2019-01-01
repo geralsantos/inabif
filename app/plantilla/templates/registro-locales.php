8fa6d3aafec2b4ab0574799eb7e1d534796506d4
geral <geral_366@hotmail.com>
2018-12-31 19:47:53 -0500

    update

--- app/plantilla/templates/registro-locales.php
+++ app/plantilla/templates/registro-locales.php
@@ -99,11 +99,9 @@
                             
                         </div>
                         <div class="row">
-                                <div class="form-group col-md-6">
-                                    <div class=" "><label for="text-input" class=" form-control-label">Nombre del Servicio</label>
-                                    <input type="text" v-model="nom_serv" name="nom_serv" placeholder="" class="form-control"> 
-                                    </div>
-                                </div>
+                            <div class="form-group col-md-6">
+                                <label for="text-input" class=" form-control-label">Nombre del Servicio</label>
+                                <input type="text" v-model="nom_serv" name="nom_serv" placeholder="" class="form-control"> 
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="text-input" class=" form-control-label">Departamento de Nacimiento</label>
