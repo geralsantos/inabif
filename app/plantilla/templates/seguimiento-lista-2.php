d60d99d9dedbc30090e1444cc96e85971fe94353
geralsantos <geral_366@hotmail.com>
2018-12-31 20:10:44 -0500

    update

--- app/plantilla/templates/seguimiento-lista-2.php
+++ app/plantilla/templates/seguimiento-lista-2.php
@@ -24,10 +24,10 @@
                             </thead>
                             <tbody class="text-center">
                                 <tr v-for="grupo in grupos">
-                                    <td>grupo.MODULO_NOMBRE</td>
-                                    <td>grupo.ENCARGADO_NOMBRE</td>
+                                    <td>{{grupo.MODULO_NOMBRE}}</td>
+                                    <td>{{grupo.ENCARGADO_NOMBRE}}</td>
                                     <td>{{(isempty(grupo.ESTADO_COMPLETO))?'NO':'SI'}}</td> <!-- SI O NO -->
-                                    <td>grupo.FECHA_EDICION</td>
+                                    <td>{{grupo.FECHA_EDICION}}</td>
                                     <td class="btn btn-primary" @click="mostrar_modulo(grupo.ID_MODULO)">Ver</td>
                                     <td v-if="completado"><input type="checkbox" v-model=""></td>
                                     <td v-if="!isempty(grupo.ESTADO_COMPLETO)"><label class="form-control-label">Completado</label><input type="checkbox" class="form-control" @change="completar_grupo(grupo.ID_MODULO)" :checked="!isempty(grupo.ESTADO_COMPLETO)" class="form-control" ></td>
