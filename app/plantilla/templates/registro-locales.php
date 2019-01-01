b7df52f2366845fa4accc2ab7bf9a27d87dcdcdb
geral <geral_366@hotmail.com>
2018-12-31 19:45:42 -0500

    update

--- app/plantilla/templates/registro-locales.php
+++ app/plantilla/templates/registro-locales.php
@@ -92,26 +92,26 @@
                                 </select>
                                 </div>
                             </div> 
+                            <div class="form-group col-md-6">
+                                <div class=" "><label for="text-input" class=" form-control-label">Código de Servicio</label>
+                                <input type="text" v-model="cod_serv" name="cod_serv" placeholder="" class="form-control"> </div>
+                            </div>
                         </div>
                         <div class="row">
-                                <div class="form-group col-md-6">
-                                    <div class=" "><label for="text-input" class=" form-control-label">Código de Servicio</label>
-                                    <input type="text" v-model="cod_serv" name="cod_serv" placeholder="" class="form-control"> </div>
-                                </div>
                                 <div class="form-group col-md-6">
                                     <div class=" "><label for="text-input" class=" form-control-label">Nombre del Servicio</label>
                                     <input type="text" v-model="nom_serv" name="nom_serv" placeholder="" class="form-control"> 
                                     </div>
                                 </div>
                             </div>
-                        <div class="row">
-                        <div class="form-group col-md-4">
-                                <div class=" "><label for="text-input" class=" form-control-label">Departamento de Nacimiento</label>
+                            <div class="form-group col-md-6">
+                                <label for="text-input" class=" form-control-label">Departamento de Nacimiento</label>
                                 <select name="Depatamento_Procedencia" v-model="Depatamento_Procedencia" @change="buscar_provincias()" class="form-control">
                                     <option v-for="departamento in departamentos" :value="departamento.CODDEPT">{{departamento.NOMDEPT}}</option>
                                 </select>
-                                </div>
                             </div>
+                        <div class="row">
+                            
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Provincia de Nacimiento</label>
                                 <select name="Provincia_Procedencia" v-model="Provincia_Procedencia" @change="buscar_distritos()" class="form-control">
