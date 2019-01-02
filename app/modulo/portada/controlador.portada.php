f01ac0629eb754ee34f172aef8c6e9b996e952a4
geral <geral_366@hotmail.com>
2019-01-01 22:24:52 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -346,7 +346,7 @@ class portada extends App{
   public function buscar_grupos(){
     $modelo = new modeloPortada();
     $id_centro = $_POST["id_centro"];
-      $sql = "select m.nombre as modulo_nombre,m.id as id_modulo, m.encargado_id,usu.nombre as encargado_nombre, md.estado_completo,md.fecha_edicion,m.nombre_tabla from modulos m 
+      $sql = "select distinct m.nombre as modulo_nombre,m.id as id_modulo, m.encargado_id,usu.nombre as encargado_nombre, md.estado_completo,md.fecha_edicion,m.nombre_tabla from modulos m 
       left join modulos_detalle md on (md.modulo_id=m.Id) 
       left join usuarios usu on (usu.id = m.encargado_id) 
       left join centro_atencion ca on (ca.tipo_centro_id=m.centro_id) 
