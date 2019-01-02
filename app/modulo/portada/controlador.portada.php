cbf04113b23247c1f75b493c8e37e3484ef538e4
geral <geral_366@hotmail.com>
2019-01-02 10:05:33 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -453,8 +453,79 @@ class portada extends App{
 		$grupos = $modelo->executeQuery($grupos);
 
 		$grupo_html = "<table>";
-        
+      foreach ($grupos as $key => $grupo) 
+      {
+        if ($key==0) {
+          $keys = array_keys($grupo);
+          $grupo_html .="<tr><th></th>";
+          foreach ($keys as $key) 
+          {
+            $grupo_html .="<th>$key</th>";
+          }
+          $grupo_html .="</tr>";
+        }
+		$grupo_values = array_values($grupo);
 		
+        $grupo_html .= "<tr><td></td>";
+        foreach ($grupo_values as $key => $value) {
+          $grupo_html .="<td>".$value."</td>";
+        }
+        $grupo_html .= "</tr>";
+	  }
+      $modulo_html .=$grupo_html;
+    }
+    $modulo_html .="</table>";
+    $table = '<table><tr><td>'.$centro_html.'</td></tr><tr><td>'.$modulo_html.'</td></tr></table>';
+
+    if ($modulos) 
+    {
+      echo json_encode(array("data"=>$table) ) ;
+    }else{
+      return false;
+    }
+  }
+  public function descargar_reporte_matriz_rub(){
+    $modelo = new modeloPortada();
+    $tipo_centro = $_SESSION["usuario"][0]["TIPO_CENTRO_ID"];
+    $periodo = $_POST["periodo"];
+    $matriz_id = $_POST["matriz_id"];
+    if ($periodo=="mensual") {
+      $fecha = " md.periodo_mes = ".date("m")." "; 
+    }else {
+      if (floatval(date("m")) <= 6 ) {
+        $semestral = " md.periodo_mes >= 1 AND md.periodo_mes <= 6 ";
+      }else{
+        $semestral = " md.periodo_mes >= 7 AND md.periodo_mes <= 12 ";
+      }
+      $fecha = " BETWEEN $semestral ";
+    }
+    $centro_html = "<table>";
+    $centro_html .="<tr><th>Nombre del Centro</th><th>Tipo de Centro</th><th>Fecha Matriz </th></tr>";
+
+    $centros = "select distinct ca.nom_ca as nombre_centro,ca.tipo_centro_id,tc.nombre as nombre_tipo_centro,cad.fecha_matriz from centro_atencion_detalle cad 
+    left join centro_atencion ca on(ca.id=cad.centro_id) 
+    left join tipo_centro tc on(ca.tipo_centro_id=tc.id) 
+      where cad.id = ".$matriz_id."  order by cad.id desc";
+    $centros = $modelo->executeQuery($centros);
+
+    $centro_html .="<tr><th>".$centros[0]["NOMBRE_CENTRO"]."</th><th>".$centros[0]["NOMBRE_TIPO_CENTRO"]."</th><th>".$centros[0]["FECHA_MATRIZ"]."</th></tr></table>";
+    
+    $modulo_html = "<table>";
+    $modulos = "select m.nombre as nombre_modulo,usu.nombre as nombre_usuario,md.periodo_mes,m.nombre_tabla from modulos_detalle md 
+    left join modulos m on(m.id=md.modulo_id) 
+    left join usuarios usu on(usu.id=m.encargado_id) 
+      where m.centro_id in (".$centros[0]["TIPO_CENTRO_ID"].") and ".$fecha." and md.periodo_anio = ".date("Y")." order by md.id desc";
+    $modulos = $modelo->executeQuery($modulos);
+    
+    foreach ($modulos as $key => $modulo) 
+    {
+		$modulo_html .="<tr><th></th><th>Nombre del Modulo</th><th>Encargado</th><th>Periodo Mes</th></tr>";
+		$modulo_html .="<tr><td></td><td>".$modulo["NOMBRE_MODULO"]."</td><td>".$modulo["NOMBRE_USUARIO"]."</td><td>".$modulo["PERIODO_MES"]."</td></tr>";
+
+		$grupos = "select * from (select distinct * from ".$modulo["NOMBRE_TABLA"]." order by id desc) WHERE ROWNUM = 1";
+		$grupos = $modelo->executeQuery($grupos);
+
+		$grupo_html = "<table>";
       foreach ($grupos as $key => $grupo) 
       {
         if ($key==0) {
