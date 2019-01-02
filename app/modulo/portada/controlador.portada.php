08db2057e11177e79cb20cf0387177410decdff2
geral <geral_366@hotmail.com>
2019-01-02 09:39:22 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -447,6 +447,7 @@ class portada extends App{
     
     foreach ($modulos as $key => $modulo) 
     {
+		$modulo_html .="<tr><th></th><th>Nombre del Modulo</th><th>Encargado</th><th>Periodo Mes</th></tr>";
       $modulo_html .="<tr><td></td><td>".$modulo["NOMBRE_MODULO"]."</td><td>".$modulo["NOMBRE_USUARIO"]."</td><td>".$modulo["PERIODO_MES"]."</td></tr>";
 
       $grupos = "select * from ".$modulo["NOMBRE_TABLA"]." order by id desc";
