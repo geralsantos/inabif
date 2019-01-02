03bd2f7e29bf9d126d60d325d618707283d7a00b
geralsantos <geral_366@hotmail.com>
2019-01-02 16:11:17 -0500

    update

---
+++ app/plantilla/templates/cargar-archivos.php
@@ -0,0 +1,90 @@
+<template id="carga-archivos">
+  
+    <div class="content mt-3">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                   
+                    <strong>Formulario de Carga de Documentos</strong>
+                </div>
+                <div class="card-body card-block">
+                    <button class="btn btn-success" @click="mostrar_formulario('')" >Adjuntar Nuevo</button>
+</br>
+                <table id="bootstrap-data-table" class="table table-striped table-bordered text-center">
+                    <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Fecha</th>
+                        <th>Acción</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="archivo in archivos">
+                        <td>{{archivo.NOMBRE}}</td>
+                        <td>{{archivo.FECHA}}</td>
+                        <td><button  class="btn btn-primary" @click="descargar(archivo.ID)">Descargar</button> </td>
+                        <td><button  class="btn btn-danger" @click="eliminar(archivo.ID)">Eliminar</button> </td>
+                    </tr>
+                    </tbody>
+                </table>
+                    
+                </div>
+            </div>
+        </div>
+        <div v-if="showModal">
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Formulario de carga</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" @click="showModal = false">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <form  class="form-horizontal" enctype="multipart/form-data" id="formuploadajax" v-on:submit.prevent="guardar">
+                        <div class="row">
+                        <div class="form-group col-md-4">
+
+                        <label for="text-input" class=" form-control-label">Adjuntar archivo</label>
+
+                        <input type="file" id="archivo" v-model="archivo" name="archivo" value="archivo" class="form-control-file">
+                        </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <div class=" "><label for="text-input" class=" form-control-label">Correo</label>
+                                <input type="email" v-model="Correo" name="Correo" placeholder="" class="form-control"> 
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6"><label for="text-input" class=" form-control-label">Nivel Usuario</label>
+                                <select name="nivel_id" v-model="nivel_id" class="form-control" @change="verificar_nivel()">
+                                    <option v-for="nivel in niveles_usuarios" :value="nivel.ID">{{nivel.NOMBRE}}</option>
+                                </select>
+                                </div>
+                       
+                        <div class="row">
+                            <div class="col-md-12 text-center" >
+                                <button type="submit" class="btn btn-success btn-sm">
+                                    <i class="fa fa-send"></i> Grabar datos
+                                </button>
+                            </div>
+                        </div>
+                        </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" @click="showModal = false">Cerrar</button>
+                </div>
+            </div>
+        </div>
+
+        </div>
+      </div>
+    </transition>
+  </div>
+    </div> <!-- .content -->
+    
+</template>
---
+++ js/componentes/portada/cargar-archivos.js
@@ -0,0 +1,104 @@
+Vue.component('cagar-archivos', {
+    template:'#registro-perfiles',
+    data:()=>({
+        
+        showModal: false,
+       
+    }),
+    created:function(){
+    },
+    mounted:function(){
+        this.buscar_centros();
+        this.listar_usuarios();
+        this.listar_nivelesusuarios();
+    },
+    updated:function(){
+    },
+    methods:{
+        guardar(){
+            var formData = new FormData(document.getElementById("formuploadajax"))
+            formData.append("archivo",document.getElementById('archivo'));
+            formData.append("anio",(this.anio));
+            formData.append("mes",(this.mes));
+            this.$http.post('adjuntar_archivo?view',formData,{headers: {'Content-Type': 'multipart/form-data'}}).then(function(response){
+            });
+        },
+        eliminar(usuario){
+            swal({
+                title: "Estás seguro?",
+                text: "Desea Eliminar el usuario seleccionado: "+usuario.NOMBRE,
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+              })
+              .then((willDelete) => {
+                if (willDelete) {
+                    let where = {id:usuario.ID}
+                    this.$http.post('delete_datos?view',{tabla:'usuarios',where:where}).then(function(response){
+                        if( response.body.resultado ){
+                            swal("Usuario Eliminado!", {
+                                icon: "success",
+                              });
+                            this.listar_usuarios();
+                        }else{
+                        swal("", "Un error ha ocurrido", "error");
+                        }
+                    });
+                }
+              });
+            
+        },
+        verRegistro(usuario){
+            if (isempty(usuario)) {
+                    this.id_usuario=null;
+                    this.Apellido= null;
+                    this.Nombre= null;
+                    this.Correo= null;
+                    this.DNI=null;
+                    this.NumCel= null;
+                    this.centro_id =null;
+                    this.nivel_id =null;
+                    this.usuario=null;
+                    this.clave=null;
+                    this.cclave=null;
+                    this.showModal = true;
+                    this.mostrar = true;
+            }else{
+                console.log(usuario);
+ //let  where = {'id':usuario.ID};
+              //this.$http.post('buscar?view',{where:where}).then(function(response){
+                    //this.registro = response.body.atributos[0];
+                    this.id_usuario=usuario.ID;
+                    this.Apellido= usuario.APELLIDO;
+                    this.Nombre= usuario.NOMBRE;
+                    this.Correo= usuario.CORREO;
+                    this.DNI=usuario.DNI;
+                    this.NumCel= usuario.NUMCEL;
+                    this.centro_id =usuario.CENTRO_ID,
+                    this.nivel_id =usuario.NIVEL,
+                    this.usuario=usuario.USUARIO;
+                    this.clave=usuario.CLAVE;
+                    this.showModal = true;
+              //});
+            }
+           
+          },
+        buscar_centros(){
+            this.$http.post('buscar?view',{tabla:'centro_atencion'}).then(function(response){
+                if( response.body.data ){
+                    this.centros= response.body.data;
+                }
+
+            });
+        },
+        listar_usuarios(){
+            this.$http.post('listar_usuarios?view').then(function(response){
+                if( response.body.data ){
+                    this.usuarios= response.body.data;
+                }
+
+            });
+        },
+
+    }
+  })
