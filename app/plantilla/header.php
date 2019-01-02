a7404cc6841416d144c57ca2d7d4d9720d28cdc9
geral <geral_366@hotmail.com>
2019-01-02 01:10:52 -0500

    update

--- app/plantilla/header.php
+++ app/plantilla/header.php
@@ -174,7 +174,6 @@
         }else if ($nivelusu==REGISTRADOR) {
             ?>
             <h3 class="menu-title">REGISTRO</h3>
-            {{changeview}}
             <modulos :changeviewevent="changeview"></modulos> 
             <h3 class="menu-title">SEGUIMIENTO</h3>
             <h3 class="menu-title">REPORTES</h3>
@@ -183,7 +182,7 @@
         }
         
         ?>
-           
+           <modulos :changeviewevent="changeview"></modulos> 
 
             </ul>
         </div><!-- /.navbar-collapse -->
