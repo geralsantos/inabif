2200c0bdf07ba584f450eabe8f71b96905856cbe
geralsantos <geral_366@hotmail.com>
2019-01-02 13:33:49 -0500

    update

--- app/plantilla/templates/nna-actividades-sociorecreativas.php
+++ app/plantilla/templates/nna-actividades-sociorecreativas.php
@@ -7,9 +7,15 @@
                     <h6>Formulario de Carga de Datos</h6>
                 </div>
                 <div class="card-body card-block">
+                    <div class="row form-group">
+                    <div class="col-12 col-md-1">
+                                    <label for="text-input" class=" form-control-label"></label>
+                                    <button @click="mostrar_lista_residentes()" class="btn btn-primary"><i class="fa fa fa-users"></i> Lista</button>
+                                </div>
+                    </div>
                     <form class="form-horizontal" v-on:submit.prevent="guardar">
                         <div class="row">
-                            <div class="form-group col-md-6 ">
+                            <div class="form-group col-md-7 ">
                                 <label for="text-input" class=" form-control-label">Nombre Residente</label>
                                 <div class="autocomplete">
                                     <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
@@ -33,7 +39,7 @@
                                 <option value="2021">2021</option>
                                 </select>
                             </div>
-                            <div class="form-group col-md-2">
+                            <div class="form-group col-md-3">
                                 <div class=""><label for="text-input" class=" form-control-label">Mes</label>
                                 <select id="mes" v-model="mes" disabled="disabled" class="form-control" >
                                         <option value="1">Enero</option>
@@ -50,10 +56,7 @@
                                         <option value="12">Diciembre</option>
                                     </select> </div>
                             </div>
-                            <div class="col-12 col-md-1">
-                                    <label for="text-input" class=" form-control-label"></label>
-                                    <button @click="mostrar_lista_residentes()" class="btn btn-primary"><i class="fa fa fa-users"></i> Lista</button>
-                                </div>
+                           
 
                         </div>
                         <div class="row">
