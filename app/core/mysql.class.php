87a35f2d4bf48c4fdf423af0cc36edddc3d299fa
geral <geral_366@hotmail.com>
2019-01-02 16:07:44 -0500

    update

--- app/core/mysql.class.php
+++ app/core/mysql.class.php
@@ -180,12 +180,15 @@ class MySQL extends PDO
             $coma = '';
             foreach($values as $key => $val){
                 $queryKeys .= $coma.$key;
-              
+                if (strpos(strtoupper($key),"FECHA")!==false) {
+                    $queryValues .= $coma." to_date('".$val."','YYYY-MM-DD') ";
+                }else{
                     $params[':'.$key] = $val;
                     $queryValues .= $coma.':'.$key;
+                }
                 $coma = ',';
             }
-             $query .= '('.$queryKeys.') VALUES ('.$queryValues.')';
+            $query .= '('.$queryKeys.') VALUES ('.$queryValues.')';
             $stmt = parent::prepare($query);
             $stmt->execute($params);
             if($stmt->rowCount()>0){
