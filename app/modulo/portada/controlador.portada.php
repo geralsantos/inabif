3b2109fac3152b35a6ecbaa65257fce4244ff2b5
geral <geral_366@hotmail.com>
2019-01-01 20:32:41 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -363,7 +363,7 @@ class portada extends App{
   }
   public function listar_usuarios(){
     $modelo = new modeloPortada();
-    $sql = "select usu.*,nu.nombre as nivel_nombre from usuarios usu left join niveles_usuarios nu on (usu.nivel=nu.id) where  to_char(fecha_creacion, 'DD-MON-YY') ='".date("y-M-d")."'";
+    $sql = "select usu.*,nu.nombre as nivel_nombre from usuarios usu left join niveles_usuarios nu on (usu.nivel=nu.id) where  to_char(usu.fecha_creacion, 'DD-MON-YY') ='".date("y-M-d")."'";
 
     $res = $modelo->executeQuery($sql );
     if ($res) 
