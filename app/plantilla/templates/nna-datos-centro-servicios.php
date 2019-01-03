30986420f078bacb097794711d21500d4e0b3ce1
lvillalobos <lvillalobos@aeonlinesolutions.com>
2019-01-03 16:32:13 -0500

    update

--- app/plantilla/templates/nna-datos-centro-servicios.php
+++ app/plantilla/templates/nna-datos-centro-servicios.php
@@ -7,29 +7,10 @@
                     <h6>Formulario de Carga de Datos</h6>
                 </div>
                 <div class="card-body card-block">
-                    <div class="row form-group">
-                        <div class="col-12 col-md-1">
-                            <label for="text-input" class=" form-control-label"></label>
-                            <button @click="mostrar_lista_residentes()" class="btn btn-primary"><i class="fa fa fa-users"></i> Lista de Residentes</button>
-                        </div>
-                    </div>
+
                     <form class="form-horizontal" v-on:submit.prevent="guardar">
                         <div class="row">
-                            <div class="form-group col-md-7">
-                                <label for="text-input" class=" form-control-label">Nombre Residente</label>
-                                <div class="autocomplete">
-                                    <input type="text"  v-model="nombre_residente" class="form-control" @keyup="buscar_residente()" placeholder="Nombre, Apellido o DNI"/>
-                                    <ul  id="autocomplete-results" class="autocomplete-results" v-if="bloque_busqueda">
-                                        <li class="loading" v-if="isLoading">
-                                            Loading results...
-                                        </li>
-                                        <li  @click="actualizar(coincidencia)" class="autocomplete-result" v-for="coincidencia in coincidencias">
-                                            {{coincidencia.NOMBRE}} {{coincidencia.APELLIDO_P}} - {{coincidencia.DOCUMENTO}}
-                                        </li>
 
-                                    </ul>
-                                </div>
-                            </div>
                             <div class="form-group col-md-2">
                                 <label for="text-input" class=" form-control-label">Año</label>
                                 <select name="select" disabled="disabled" id="anio"  v-model="anio" class="form-control">
@@ -142,51 +123,6 @@
                         </form>
                 </div>
             </div>
-        </div>
-        <div v-if="modal_lista">
-             <transition name="modal">
-                <div class="modal-mask">
-                    <div class="modal-wrapper">
-
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title">Lista Residentes</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true" @click="modal_lista = false">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="table-responsive">
-                                    <table class="table">
-                                        <thead class="thead-dark text-center">
-                                            <tr>
-                                                <th scope="col">Nombre</th>
-                                                <th scope="col">Número de Documento</th>
-                                                <th scope="col">Opción</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody class="text-center">
-                                            <tr v-for="residente in pacientes">
-                                                <td>{{residente.NOMBRE}} {{residente.APELLIDO}}</td>
-                                                <td>{{residente.DOCUMENTO}}</td>
-                                                <td><button class="btn btn-primary" @click="elegir_residente(residente)">Seleccionar</button></td>
-
-                                            </tr>
-
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" @click="modal_lista = false">Cerrar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    </div>
-                </div>
-            </transition>
         </div>
     </div> <!-- .content -->
 </template>
--- js/componentes/portada/nna-datos-centro-servicios.js
+++ js/componentes/portada/nna-datos-centro-servicios.js
@@ -44,103 +44,7 @@ Vue.component('nna-datos-centro-servicios', {
         }
     },
     methods:{
-        guardar(){
-            if (this.id_residente==null) {
-                swal('Error', 'Residente no existe', 'warning');
-                return false;
-            }
-            let valores = {
 
-                Cod_Entidad:this.Cod_Entidad,
-                Nom_Entidad:this.Nom_Entidad,
-                Cod_Linea:this.Cod_Linea,
-                Nom_Linea:this.Nom_Linea,
-                Linea_Intervencion:this.Cod_Entidad,
-                Cod_Servicio:this.Cod_Servicio,
-                NomC_Servicio:this.NomC_Servicio,
-                Ubigeo:this.Departamento_Centro + this.Provincia_centro + this.Departamento_Centro,
-                Departamento_Centro :this.Departamento_Centro,
-                Provincia_centro:this.Provincia_centro,
-                Distrito_centro :this.Distrito_centro,
-                Area_Residencia :this.Area_Residencia,
-                CodigoC_Atencion :this.CodigoC_Atencion,
-                NomC_Atencion :this.NomC_Atencion,
-                Fecha_Registro :moment().format("YY-MMM-DD"),
-
-                Residente_Id: this.id_residente,
-                Periodo_Mes: moment().format("MM"),
-                Periodo_Anio:moment().format("YYYY")
-
-            }
-
-            this.$http.post('insertar_datos?view',{tabla:'NNACentroServicios', valores:valores}).then(function(response){
-
-                if( response.body.resultado ){
-                    swal('', 'Registro Guardado', 'success');
-
-                }else{
-                  swal("", "Un error ha ocurrido", "error");
-                }
-            });
-        },
-        buscar_residente(){
-            this.id_residente = null;
-
-            var word = this.nombre_residente;
-            if( word.length >= 4){
-                this.coincidencias = [];
-                this.bloque_busqueda = true;
-                this.isLoading = true;
-
-                this.$http.post('ejecutar_consulta?view',{like:word }).then(function(response){
-
-                    if( response.body.data != undefined){
-                        this.isLoading = false;
-                        this.coincidencias = response.body.data;
-                    }else{
-                        this.bloque_busqueda = false;
-                        this.isLoading = false;
-                        this.coincidencias = [];
-                    }
-                 });
-            }else{
-                this.bloque_busqueda = false;
-                this.isLoading = false;
-                this.coincidencias = [];
-            }
-        },
-        actualizar(coincidencia){
-            this.id_residente = coincidencia.ID;
-            let nombre=(coincidencia.NOMBRE==undefined)?'':coincidencia.NOMBRE;
-let apellido_p = (coincidencia.APELLIDO_P==undefined)?'':coincidencia.APELLIDO_P;
-let apellido_m = (coincidencia.APELLIDO_M==undefined)?'':coincidencia.APELLIDO_M;
-let apellido = apellido_p + ' ' + apellido_m;
- this.nombre_residente=nombre + ' ' + apellido;
-            this.coincidencias = [];
-            this.bloque_busqueda = false;
-
-            this.$http.post('cargar_datos_residente?view',{tabla:'NNACentroServicios', residente_id:this.id_residente }).then(function(response){
-
-                if( response.body.atributos != undefined){
-
-                    this.Cod_Entidad = response.body.atributos[0]["COD_ENTIDAD"];
-                    this.Nom_Entidad = response.body.atributos[0]["NOM_ENTIDAD"];
-                    this.Cod_Linea = response.body.atributos[0]["COD_LINEA"];
-                    this.Nom_Linea = response.body.atributos[0]["NOM_LINEA"];
-                    this.Linea_Intervencion = response.body.atributos[0]["LINEA_INTERVENCION"];
-                    this.Cod_Servicio = response.body.atributos[0]["COD_SERVICIO"];
-                    this.NomC_Servicio = response.body.atributos[0]["NOMC_SERVICIO"];
-                    this.Departamento_Centro = response.body.atributos[0]["DEPARTAMENTO_CENTRO"];
-                    this.Provincia_centro = response.body.atributos[0]["PROVINCIA_CENTRO"];
-                    this.Distrito_centro = response.body.atributos[0]["DISTRITO_CENTRO"];
-                    this.Area_Residencia = response.body.atributos[0]["AREA_RESIDENCIA"];
-                    this.CodigoC_Atencion = response.body.atributos[0]["CODIGOC_ATENCION"];
-                    this.NomC_Atencion = response.body.atributos[0]["NOMC_ATENCION"];
-
-                }
-             });
-
-        },
         buscar_departamentos(){
             this.$http.post('buscar_departamentos?view',{tabla:'ubigeo'}).then(function(response){
                 if( response.body.data ){
@@ -232,7 +136,7 @@ let apellido = apellido_p + ' ' + apellido_m;
             this.$http.post('buscar_centro?view',{tabla:'centro_atencion' }).then(function(response){
 
                 if( response.body.data != undefined){
-                    console.log(response.body.data[0])
+
                     let ubigeo =  response.body.atributos[0]["UBIGEO"];
                     let departamento = ubigeo.substring(0, 2);
                     let provincia = ubigeo.substring(0, 4);
@@ -240,7 +144,6 @@ let apellido = apellido_p + ' ' + apellido_m;
                     this.Cod_Entidad = response.body.atributos[0]["CODIGO_ENTIDAD"];
                     this.Nom_Entidad = response.body.atributos[0]["NOMBRE_ENTIDAD"];
                     this.Cod_Linea = response.body.atributos[0]["CODIGO_LINEA"];
-
                     this.Linea_Intervencion = response.body.atributos[0]["LINEA_INTERVENCION"];
                     this.Cod_Servicio = response.body.atributos[0]["COD_SERV"];
                     this.NomC_Servicio = response.body.atributos[0]["NOM_SERV"];
