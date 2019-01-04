0ac858e1c558a92b554c2be89abcf313f18313e4
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-04 17:55:40 -0500

    update

--- app/plantilla/templates/seguimiento-lista-1.php
+++ app/plantilla/templates/seguimiento-lista-1.php
@@ -30,7 +30,7 @@
                                         <td>{{(isempty(centro.FECHA_CIERRE))?'NO':'SI'}}</td>
                                         <td><button class="btn btn-primary" @click="ver_grupos(centro.ID_CENTRO)">Ver</button></td>
                                         <td v-if="isempty(centro.ESTADO_COMPLETO) && mostrar_completado"><label class="form-control-label">Completado</label><input type="checkbox" class="form-control" @change="completar_matriz(centro.ID_CENTRO)" :checked="!isempty(centro.ESTADO_COMPLETO)" :disabled="" class="form-control" ></td>
-                                        <td v-if="isempty(centro.ESTADO_COMPLETO) && mostrar_completado"> <button class="btn btn-success" @click="generar_matriz(centro.ID_CENTRO)">Generar Matriz</button></td>
+                                        <td v-if="!isempty(centro.ESTADO_COMPLETO) && mostrar_completado"> <button class="btn btn-success" @click="generar_matriz(centro.ID_CENTRO)">Generar Matriz</button></td>
                                     </tr>
 
                                 </tbody>
