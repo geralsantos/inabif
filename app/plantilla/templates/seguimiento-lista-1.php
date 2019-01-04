241aedcbddde2302e701cdfaf63db143afdc746b
lvillalobos <lvillalobos@aeonlinesolutions.com>
2019-01-04 12:21:10 -0500

    update

--- app/plantilla/templates/seguimiento-lista-1.php
+++ app/plantilla/templates/seguimiento-lista-1.php
@@ -18,7 +18,7 @@
                                         <th scope="col">Fecha Cierre</th>
                                         <th scope="col">Cerrado</th>
                                         <th scope="col">Ver</th>
-                                        <th scope="col" v-if="mostrar_completado">Acciones</th>
+                                        <th scope="col" v-if="mostrar_completado">Opción</th>
 
                                     </tr>
                                 </thead>
@@ -29,7 +29,7 @@
                                         <td>{{centro.FECHA_CIERRE}}</td>
                                         <td>{{(isempty(centro.FECHA_CIERRE))?'NO':'SI'}}</td>
                                         <td><button class="btn btn-primary" @click="ver_grupos(centro.ID_CENTRO)">Ver</button></td>
-                                        <td><label class="form-control-label">Completado</label><input type="checkbox" class="form-control" @change="completar_matriz(centro.ID_CENTRO)" :checked="!isempty(centro.ESTADO_COMPLETO)" :disabled="isempty(centro.ESTADO_COMPLETO) && mostrar_completado" class="form-control" ></td>
+                                        <td v-if="isempty(centro.ESTADO_COMPLETO) && mostrar_completado"><label class="form-control-label">Completado</label><input type="checkbox" class="form-control" @change="completar_matriz(centro.ID_CENTRO)" :checked="!isempty(centro.ESTADO_COMPLETO)" :disabled="" class="form-control" ></td>
                                         <td v-if="!isempty(centro.ESTADO_COMPLETO) && mostrar_completado"> <button class="btn btn-success" @click="generar_matriz(centro.ID_CENTRO)">Generar Matriz</button></td>
                                     </tr>
 
