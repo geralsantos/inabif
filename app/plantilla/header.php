a93b684ac324792c2a946a78db699e8d434be271
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-04 17:20:31 -0500

    update

--- app/plantilla/header.php
+++ app/plantilla/header.php
@@ -191,6 +191,7 @@
             <h3 class="menu-title">REGISTRO</h3>
            <modulos :changeviewevent="changeview"></modulos> 
             <h3 class="menu-title">SEGUIMIENTO</h3>
+            <a href="#seguimiento-lista-1" class="list-group-item level-0" aria-expanded="true" id="Perfiles" style="width:100%;">Matrices en los Centros</a>
             <h3 class="menu-title">REPORTES</h3>
             <h3 class="menu-title">PERFILES</h3>
             <?php 
