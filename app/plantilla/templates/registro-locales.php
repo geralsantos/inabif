88aa55e929a18752526e8002e25261114d105540
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-10 09:41:28 -0500

    update

--- app/plantilla/templates/registro-locales.php
+++ app/plantilla/templates/registro-locales.php
@@ -104,48 +104,25 @@
                                 <input type="text" v-model="nom_serv" name="nom_serv" placeholder="" class="form-control"> 
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="text-input" class=" form-control-label">Departamento de Nacimiento</label>
-                                <select name="Depatamento_Procedencia" v-model="Depatamento_Procedencia" @change="buscar_provincias()" class="form-control">
-                                    <option v-for="departamento in departamentos" :value="departamento.CODDEPT">{{departamento.NOMDEPT}}</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row">
-                            
-                            <div class="form-group col-md-6">
-                                <div class=" "><label for="text-input" class=" form-control-label">Provincia de Nacimiento</label>
-                                <select name="Provincia_Procedencia" v-model="Provincia_Procedencia" @change="buscar_distritos()" class="form-control">
-                                    <option v-for="provincia in provincias" :value="provincia.CODPROV">{{provincia.NOMPROV}}</option>
-                                </select>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-6">
-                                <div class=" "><label for="text-input" class=" form-control-label">Distrito de Nacimiento</label>
-                                <select name="Distrito_Procedencia" v-model="Distrito_Procedencia" class="form-control">
-                                    <option v-for="distrito in distritos" :value="distrito.CODDIST">{{distrito.NOMDIST}}</option>
-                                </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group col-md-6">
                                 <div class=" "><label for="text-input" class=" form-control-label">Nombre de Administrador</label>
                                 <input type="text" v-model="administrador_nombre" name="administrador_nombre" placeholder="" class="form-control"> 
                                 </div>
                             </div>
-                            <div class="form-group col-md-6">
+                        </div>
+                        
+                        <div class="row">
+                        <div class="form-group col-md-6">
                                 <div class=" "><label for="text-input" class=" form-control-label">Nombre de Director</label>
                                 <input type="text" v-model="nombre_director" name="nombre_director" placeholder="" class="form-control"> 
                                 </div>
                             </div>
-                            
-                        </div>
-                        <div class="row">
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Teléfono</label>
                                 <input type="number" v-model="telefono" name="telefono" placeholder="" class="form-control"> 
                                 </div>
                             </div>
+                        </div>
+                        <div class="row">
                             <div class="form-group col-md-8">
                                 <div class=" "><label for="text-input" class=" form-control-label">Dirección del Centro</label>
                                 <input type="text" v-model="direccion_car" name="direccion_car" placeholder="" class="form-control"> 
@@ -154,6 +131,29 @@
                             
                         </div>
                         <div class="row">
+                        <div class="form-group col-md-6">
+                                <label for="text-input" class=" form-control-label">Departamento de Nacimiento</label>
+                                <select name="Depatamento_Procedencia" v-model="Depatamento_Procedencia" @change="buscar_provincias()" class="form-control">
+                                    <option v-for="departamento in departamentos" :value="departamento.CODDEPT">{{departamento.NOMDEPT}}</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class=" "><label for="text-input" class=" form-control-label">Provincia de Nacimiento</label>
+                                <select name="Provincia_Procedencia" v-model="Provincia_Procedencia" @change="buscar_distritos()" class="form-control">
+                                    <option v-for="provincia in provincias" :value="provincia.CODPROV">{{provincia.NOMPROV}}</option>
+                                </select>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="row">
+                        <div class="form-group col-md-6">
+                                <div class=" "><label for="text-input" class=" form-control-label">Distrito de Nacimiento</label>
+                                <select name="Distrito_Procedencia" v-model="Distrito_Procedencia" class="form-control">
+                                    <option v-for="distrito in distritos" :value="distrito.CODDIST">{{distrito.NOMDIST}}</option>
+                                </select>
+                                </div>
+                            </div>
                             <div class="form-group col-md-4">
                                 <div class=" "><label for="text-input" class=" form-control-label">Código de Linea </label>
                                 <input type="text" v-model="codigo_linea" name="codigo_linea" placeholder="" class="form-control"> 
