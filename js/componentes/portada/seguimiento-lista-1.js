6b1d1d60b1767a07c1ae76ea7b333f026a228ef9
lvillalobos <lvillalobos@aeonlinesolutions.com>
2019-01-10 16:49:05 -0500

    update

--- js/componentes/portada/seguimiento-lista-1.js
+++ js/componentes/portada/seguimiento-lista-1.js
@@ -120,7 +120,7 @@ let apellido = apellido_p + ' ' + apellido_m;
 
                 if( response.body.data != undefined){
                     this.usuario = response.body.data[0];
-                   if(this.usuario.NIVEL == 5 || this.usuario.NIVEL == 6){
+                   if(this.usuario.NIVEL == 5 ){
                         this.mostrar_completado = true;
                   }
 
