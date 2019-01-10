9d2271de5fce2707d67b40d2799601f94b818fc2
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-10 15:17:25 -0500

    update

--- app/plantilla/header.php
+++ app/plantilla/header.php
@@ -128,7 +128,6 @@
 </style>
         <?php
         $nivelusu = $_SESSION["usuario"][0]["NIVEL"];
-        print_r($_SESSION["usuario"][0]);
         if ($nivelusu==ADMIN_CENTRAL) {
             ?>
             <h3 class="menu-title">REGISTRO</h3>
