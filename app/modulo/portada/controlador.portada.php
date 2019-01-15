f2b593e539132ceb06fc6812cdb298d39ed5553d
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-15 17:54:32 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -501,7 +501,7 @@ class portada extends App{
     $modelo = new modeloPortada();
     $nombre_centro = $modelo->executeQuery("select * from centro_atencion where id=". $_POST["id_centro"]." and estado = 1");
     $nombre_tabla = $_POST["nombre_tabla"];
-    $sql = "select * from ".$nombre_tabla." where  to_char(fecha_creacion, 'MON-YY') =UPPER('".date("M-y")."')";
+    $sql = "select nt.*,re.nombre,re.apellido_p from ".$nombre_tabla." nt inner join residente re on (re.id=nt.residente_id) where to_char(nt.fecha_creacion, 'MON-YY') =UPPER('".date("M-y")."')";
 
     $res = $modelo->executeQuery($sql );
     if ($res)
