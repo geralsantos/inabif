6012ead81d788238cc627bcb05faf00262e02b00
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-17 17:52:30 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -623,11 +623,12 @@ class portada extends App{
 
       $last_day = date('d', strtotime("{$aux} - 1 day"));
 
+	  $fecha = " BETWEEN UPPER('".date("01-M-y",strtotime($periodo_anio."-".$periodo_mes))."') AND UPPER('".date(($last_day."-M-y"),strtotime($periodo_anio."-".$periodo_mes))."')";
 
       $centros = "select  max(ca.id),max(ca.nom_ca) as nombre_centro,max(ca.tipo_centro_id) as tipo_centro_id,max(tc.nombre) as nombre_tipo_centro,to_char(max(cad.fecha_matriz),'DD-MON-YY HH24:MI') as fecha_matriz,max(cad.id) from centro_atencion_detalle cad
       left join centro_atencion ca on(ca.id=cad.centro_id)
       left join tipo_centro tc on(ca.tipo_centro_id=tc.id)
-        where ".$matriz_id." ca.estado=1 group by ca.id order by ca.id desc";
+        where ".$matriz_id." to_char(cad.fecha_matriz,'DD-MON-YY') ".$fecha." ca.estado=1 group by ca.id order by ca.id desc";
       $centros = $modelo->executeQuery($centros);
 	  $html2 ="";
       foreach ($centros as $key => $centro) 
