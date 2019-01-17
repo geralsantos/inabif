3e42b8f016348c778bfe316fc5918bb493d05c5f
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-17 17:38:22 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -627,7 +627,7 @@ class portada extends App{
       $centros = "select  max(ca.id),max(ca.nom_ca) as nombre_centro,max(ca.tipo_centro_id) as tipo_centro_id,max(tc.nombre) as nombre_tipo_centro,to_char(max(cad.fecha_matriz),'DD-MON-YY HH24:MI') as fecha_matriz,max(cad.id), from centro_atencion_detalle cad
       left join centro_atencion ca on(ca.id=cad.centro_id)
       left join tipo_centro tc on(ca.tipo_centro_id=tc.id)
-        where ".$matriz_id." ca.estado=1 group by ca.id order by cad.id desc";
+        where ".$matriz_id." ca.estado=1 group by ca.id order by ca.id desc";
       $centros = $modelo->executeQuery($centros);
 	  $html2 ="";
       foreach ($centros as $key => $centro) 
