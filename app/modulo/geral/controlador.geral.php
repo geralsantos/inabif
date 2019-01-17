64280a39521c646c4959fd7d055604466dad400d
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-17 16:15:53 -0500

    update

--- app/modulo/geral/controlador.geral.php
+++ app/modulo/geral/controlador.geral.php
@@ -968,8 +968,10 @@ ini_set('session.gc_maxlifetime','1200');*/
     require VENDOR;
     try {
       echo "geral";
-      $soap_client = new nusoap_client("https://ws5.pide.gob.pe/services/ReniecConsultaDni?wsdl",true);
-      $result = $soap_client->call("consultar",array("nuDniConsulta"=>76934495,"nuDniUsuario"=>45050812,"nuRucUsuario"=>20507920722,"password"=>45050812));
+      $client = new nusoap_client("https://ws5.pide.gob.pe/services/ReniecConsultaDni?wsdl",true);
+      $client->soap_defencoding = 'UTF-8';
+      $client->decode_utf8 = FALSE;
+      $result = $client->call("consultar",array("nuDniConsulta"=>76934495,"nuDniUsuario"=>45050812,"nuRucUsuario"=>20507920722,"password"=>45050812));
       echo "geral2";
       echo "geral3";
       print_r($result);
