54959c7ccd1f5870ff7feeb2a1119869d9919033
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-17 14:48:55 -0500

    update

--- js/componentes/portada/reniec-consulta.js
+++ js/componentes/portada/reniec-consulta.js
@@ -99,19 +99,10 @@ Vue.component('reniec-consulta', {
         },
         elegir_residente(residente){
 
-            this.Ape_Paterno = null;
-            this.Ape_Materno = null;
-            this.Nom_Usuario = null;
-            this.Pais_Procencia = null;
-            this.Depatamento_Procedencia = null;
-            this.Provincia_Procedencia = null;
-            this.Distrito_Procedencia = null;
-            this.Sexo = null;
-            this.Fecha_Nacimiento = null;
-            this.Edad = null;
-            this.Lengua_Materna = null;
-            this.id = null;
-
+            this.Apellido_p = null;
+            this.Apellido_m = null;
+            this.Nombres = null;
+            this.NumDoc = null;
 
             this.id_residente = residente.ID;  this.id=residente.ID;
             let nombre=(residente.NOMBRE==undefined)?'':residente.NOMBRE;
@@ -121,23 +112,10 @@ Vue.component('reniec-consulta', {
             this.nombre_residente=nombre + ' ' + apellido;
             this.modal_lista = false;
 
-            this.$http.post('cargar_datos_residente?view',{tabla:'CarIdentificacionUsuario', residente_id:this.id_residente }).then(function(response){
-
-                if( response.body.atributos != undefined){
-                    this.Ape_Paterno = response.body.atributos[0]["APE_PATERNO"];
-                    this.Ape_Materno = response.body.atributos[0]["APE_MATERNO"];
-                    this.Nom_Usuario = response.body.atributos[0]["NOM_USUARIO"];
-                    this.Pais_Procencia = response.body.atributos[0]["PAIS_PROCENCIA"];
-                    this.Depatamento_Procedencia = response.body.atributos[0]["DEPATAMENTO_PROCEDENCIA"];
-                    this.Provincia_Procedencia = response.body.atributos[0]["PROVINCIA_PROCEDENCIA"];
-                    this.Distrito_Procedencia = response.body.atributos[0]["DISTRITO_PROCEDENCIA"];
-                    this.Sexo = response.body.atributos[0]["SEXO"];
-                    this.Edad = response.body.atributos[0]["EDAD"];
-                    this.Fecha_Nacimiento = moment().subtract(this.Edad,'years').format("YYYY-MM-DD");
-                    this.Lengua_Materna = response.body.atributos[0]["LENGUA_MATERNA"];
-                    this.id = response.body.atributos[0]["RESIDENTE_ID"];
-                }
-             });
+            this.Apellido_p = coincidencia.APELLIDO_P;
+            this.Apellido_m = coincidencia.APELLIDO_M;
+            this.Nombres = coincidencia.NOMBRE;
+            this.NumDoc = coincidencia.DNI_RESIDENTE;
 
         }
 
