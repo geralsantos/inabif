3f1fa98a3a55de52cfdec18e63e6050b524be9da
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-17 14:31:02 -0500

    update

--- js/componentes/portada/reniec-consulta.js
+++ js/componentes/portada/reniec-consulta.js
@@ -1,26 +1,6 @@
 Vue.component('reniec-consulta', {
     template:'#reniec-consulta',
     data:()=>({
-        Ape_Paterno:null,
-        Ape_Materno:null,
-        Nom_Usuario:null,
-        Pais_Procencia:null,
-        Depatamento_Procedencia:null,
-        Provincia_Procedencia:null,
-        Distrito_Procedencia:null,
-        Sexo:null,
-        Fecha_Nacimiento:null,
-        Edad:null,
-        Lengua_Materna:null,
-        id:null,
-
-        paises:[],
-        departamentos:[],
-        provincias:[],
-        distritos:[],
-        lenguas:[],
-
-        nombre_residente:null,
         isLoading:false,
         mes:moment().format("M"),
         anio:(new Date()).getFullYear(),
@@ -28,8 +8,8 @@ Vue.component('reniec-consulta', {
         bloque_busqueda:false,
         id_residente:null,
         modal_lista:false,
-        pacientes:[]
-
+        pacientes:[],
+        showModal: false,
 
     }),
     created:function(){
@@ -38,113 +18,6 @@ Vue.component('reniec-consulta', {
      
     },
     methods:{
-        inicializar(){
-            this.Ape_Paterno = null;
-            this.Ape_Materno = null;
-            this.Nom_Usuario = null;
-            this.Pais_Procencia = null;
-            this.Depatamento_Procedencia = null;
-            this.Provincia_Procedencia = null;
-            this.Distrito_Procedencia = null;
-            this.Sexo = null;
-            this.Fecha_Nacimiento = null;
-            this.Edad = null;
-            this.Lengua_Materna = null;
-            this.id = null;
-
-            this.nombre_residente=null;
-            this.isLoading=false;
-            this.mes=moment().format("M");
-            this.anio=(new Date()).getFullYear();
-            this.coincidencias=[];
-            this.bloque_busqueda=false;
-            this.id_residente=null;
-            this.modal_lista=false;
-            this.pacientes = [];
-
-        },
-        guardar(){
-            /*if (this.id_residente==null) {
-                swal('Error', 'Residente no existe', 'warning');
-                return false;
-            }*/
-            let valores = {
-                Ape_Paterno: this.Ape_Paterno,
-                Ape_Materno: this.Ape_Materno,
-                Nom_Usuario: this.Nom_Usuario,
-                Pais_Procencia: this.Pais_Procencia,
-                Depatamento_Procedencia: this.Depatamento_Procedencia || null,
-                Provincia_Procedencia: this.Provincia_Procedencia || null,
-                Distrito_Procedencia: this.Distrito_Procedencia || null,
-                Sexo: this.Sexo,
-                Fecha_Nacimiento:  moment(this.Fecha_Nacimiento, "YYYY-MM-DD").format("YYYY-MM-DD"),
-                Edad: this.Edad,
-                Lengua_Materna: this.Lengua_Materna,
-
-                //Residente_Id: this.id_residente,
-                Periodo_Mes: moment().format("MM"),
-                Periodo_Anio:moment().format("YYYY")
-
-                }
-
-            /*this.$http.post('insertar_datos?view',{tabla:'CarIdentificacionUsuario', valores:valores}).then(function(response){
-
-                if( response.body.resultado ){
-                    swal('', 'Registro Guardado', 'success');
-
-                }else{
-                  swal("", "Un error ha ocurrido", "error");
-                }
-            });*/
-           /* let valores_arr = Object.values(valores);
-                for (let index = 0; index < valores_arr.length; index++) {
-                    if (isempty(valores_arr[index])) {
-                        swal('Error', 'Debe llenar todos los campos', 'warning');
-                        return false;
-                    }
-                }*/
-                if (isempty(this.id_residente)) {
-                    let valores_residente = {
-
-
-                        nombre : this.Nom_Usuario,
-                        apellido_p : this.Ape_Paterno,
-                        apellido_m : this.Ape_Materno,
-                        pais_id : this.Pais_Procencia,
-                        departamento_naci_cod : this.Depatamento_Procedencia || null,
-                        provincia_naci_cod : this.Provincia_Procedencia || null,
-                        distrito_naci_cod : this.Distrito_Procedencia || null,
-                        sexo: this.Sexo,
-                        fecha_naci :  moment(this.Fecha_Nacimiento, "YYYY-MM-DD").format("YYYY-MM-DD"),
-                        edad: this.Edad,
-                        lengua_materna: this.Lengua_Materna,
-                        //documento :this.Numero_Doc
-                        }
-                        console.log(valores_residente);
-                    this.$http.post('insertar_datos?view',{tabla:'residente', valores:valores_residente,lastid:true}).then(function(response){
-                        valores.Residente_Id = response.body.lastid;
-                        console.log(response.body.lastid);
-                        this.$http.post('insertar_datos?view',{tabla:'CarIdentificacionUsuario', valores:valores}).then(function(response){
-                            if( response.body.resultado ){
-                                this.inicializar();
-                                swal('', 'Registro Guardado', 'success');
-                            }else{
-                              swal("", "Un error ha ocurrido", "error");
-                            }
-                        });
-                    });
-                }else{
-                    valores.Residente_Id = this.id_residente;
-                    this.$http.post('insertar_datos?view',{tabla:'CarIdentificacionUsuario', valores:valores}).then(function(response){
-                        if( response.body.resultado ){
-                            this.inicializar();
-                            swal('', 'Registro Guardado', 'success');
-                        }else{
-                          swal("", "Un error ha ocurrido", "error");
-                        }
-                    });
-                }
-        },
         buscar_residente(){
             this.id_residente = null;
 
@@ -181,7 +54,7 @@ Vue.component('reniec-consulta', {
             this.coincidencias = [];
             this.bloque_busqueda = false;
 
-            this.$http.post('cargar_datos_residente?view',{tabla:'CarIdentificacionUsuario', residente_id:this.id_residente }).then(function(response){
+            /*this.$http.post('cargar_datos_residente?view',{tabla:'CarIdentificacionUsuario', residente_id:this.id_residente }).then(function(response){
 
                 if( response.body.atributos != undefined){
                     this.Ape_Paterno = response.body.atributos[0]["APE_PATERNO"];
@@ -197,7 +70,7 @@ Vue.component('reniec-consulta', {
                     this.Lengua_Materna = response.body.atributos[0]["LENGUA_MATERNA"];
                     this.id = response.body.atributos[0]["RESIDENTE_ID"];
                 }
-             });
+             });*/
 
         },mostrar_lista_residentes(){
 
