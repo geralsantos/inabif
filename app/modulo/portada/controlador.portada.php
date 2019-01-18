eb166d601e447b70c43a38863c7adf1cf64f6295
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-18 15:44:34 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -919,7 +919,8 @@ Numero_Documento as "Número documento de ingreso"',
 
     'DATOS DEL ESTADO PSICOLÓGICO DEL USUARIO'=>
     '\'\' as "Tras. comport. y/o disociales",
-    \'\' as "Tipo de transtorno"');
+    \'\' as "Tipo de transtorno"',
+'CarEgresoGeneral'=>'Fecha_Egreso as "Fecha de Egreso",Motivo_Egreso as "Motivo de Egreso",Retiro_Voluntario as "Retiro Voluntario",Reinsercion, Grado_Parentesco as "Grado de Parentesco", Traslado, Fallecimiento, Restitucion_derechos as "Restitución de Derechos",AUS,Constancia_Naci as "Constancia de Nacimiento",Carnet_CONADIS as "CONADIS",DNI as "Documento de Identidad", Restitucion','CarEgresoSalud'=>'Plan_Medico as "Plan Médico",Meta_PII as "Meta Trazada",Informe_Tecnico as "Informe Técnico Evolutivo", Des_Informe as "Desc.Técnico Evolutivo",Cumple_Plan as "Cumpl. plan de intervención",Enfermedades_Cronicas  as "Enfermedades crónicas",Especificar');
       break;
       case '2': /*pam*/
       $campos = array('pam_datos_identificacion'=>
