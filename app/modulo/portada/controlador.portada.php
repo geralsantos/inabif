c5f19b093d5aa9e4e77d1340b5232d78b35dd98a
geral <geral_366@hotmail.com>
2019-01-20 09:17:03 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -777,7 +777,7 @@ class portada extends App{
         $fecha = " BETWEEN UPPER('".date("01-M-y",strtotime($periodo_anio."-".$periodo_mes))."') AND UPPER('".date(($last_day."-M-y"),strtotime($periodo_anio."-".$periodo_mes))."')";
   
 
-        $modulos = "select cu.residente_id as \"Codigo de Residente\",cu.Ape_Paterno ,cda.Fecha_Ingreso  from 
+        $modulos = "select cu.residente_id as \"Cod.Residente\",cu.Ape_Paterno ,cda.Fecha_Ingreso  from 
         CarIdentificacionUsuario cu 
         ,CarDatosAdmision cda 
         ,residente re
@@ -786,7 +786,7 @@ class portada extends App{
         $residentes = array();
         $grupo_html = "";
         foreach ($modulos as $key => $grupo) {
-          if (!in_array($grupo["RESIDENTE_ID"],$residentes)) {
+          if (!in_array($grupo["COD.RESIDENTE"],$residentes)) {
             if ($key==0) {
               $keys = array_keys($grupo);
               $grupo_html .="<tr><th></th>";
@@ -802,7 +802,7 @@ class portada extends App{
               $grupo_html .="<td>".$value."</td>";
             }
             $grupo_html .= "</tr>";
-            $residentes[] = $grupo["RESIDENTE_ID"];
+            $residentes[] = $grupo["COD.RESIDENTE"];
           }
         }
         $table = '<table>'.$grupo_html.'</table>';
