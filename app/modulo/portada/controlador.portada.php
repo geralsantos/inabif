fb661b553f3d0fb37cc442c66c4f136b2e341fea
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-21 10:53:01 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -804,9 +804,10 @@ class portada extends App{
         foreach ($modulos as $key => $modulo)
         {
          
-           echo $modulo = is_array($modulo)?$modulo[0]:$modulo;
+            $modulo = is_array($modulo)?$modulo[0]:$modulo;
 
             $modulo = $modelo->executeQuery($modulo);
+            print_r($modulo);
             $residentes = array();
             $grupo_html = "";
             if ($modulo) {
