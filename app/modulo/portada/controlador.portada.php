98d28bdec197cb6c55c13dc45df977a79e0e7f32
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-22 11:16:48 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -1111,7 +1111,7 @@ Numero_Documento as "Número documento de ingreso"',
       (SELECT nombre FROM pam_nivel_educativo where id=pam_datosCondicionIngreso.nivel_educativo) as "Nivel Educativo",
       \'\' as "Cobertura médica",
       (SELECT nombre FROM pam_clasif_socioeconomico where id = pam_datosCondicionIngreso.SISFOH) as "Clas.Socioeconómica(SISFOH)",
-      (SELECT nombre FROM pam_tipo_seguro_salud WHERE id=pam_datosCondicionIngreso.aseguramiento_salud) as "Tipo de aseguramiento"', 'familiar_ubicados as "Cuenta con familiares",
+      aseguramiento_salud as "Tipo de aseguramiento"', 'familiar_ubicados as "Cuenta con familiares",
       tipo_parentesco as "Tipo de parentesco"'),
 
 
