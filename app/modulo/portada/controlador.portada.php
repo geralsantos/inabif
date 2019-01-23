90bcf12c8c18ddca0266a5e5d0c270072d531534
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-23 15:10:39 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -1347,7 +1347,7 @@ ini_set('session.gc_maxlifetime','1200');*/
         $contar_modulos_2++;
         if (is_array($valortabla)) {
           foreach ($valortabla as $key => $value) {
-            echo $grupos = "select ".$value.",residente_id from ".$modulo["NOMBRE_TABLA"]." where  periodo_mes=".date("n")." and periodo_anio=".date("Y")." and residente_id= ". $id_residente." and centro_id=".$centro["ID"]." order by id desc";
+            $grupos = "select ".$value.",residente_id from ".$modulo["NOMBRE_TABLA"]." where  periodo_mes=".date("n")." and periodo_anio=".date("Y")." and residente_id= ". $id_residente." and centro_id=".$centro["ID"]." order by id desc";
             $grupos = $modelo->executeQuery($grupos);
             $grupo_html = "";
             $residente_repite=array();
@@ -1387,7 +1387,7 @@ ini_set('session.gc_maxlifetime','1200');*/
             $html .= $modulo_html.$grupo_html;
           }
         }else {
-        echo  $grupos = "select ".$valortabla.",residente_id from ".$modulo["NOMBRE_TABLA"]." where  periodo_mes=".date("n")." and periodo_anio=".date("Y")." and residente_id= ". $id_residente." and centro_id=".$centro["ID"]." order by id desc";
+         $grupos = "select ".$valortabla.",residente_id from ".$modulo["NOMBRE_TABLA"]." where  periodo_mes=".date("n")." and periodo_anio=".date("Y")." and residente_id= ". $id_residente." and centro_id=".$centro["ID"]." order by id desc";
           $grupos = $modelo->executeQuery($grupos);
 
           $grupo_html = "";
