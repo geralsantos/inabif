b5ee3c8f03377541a23835a3022a773436600832
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-25 12:00:57 -0500

    update

--- app/plantilla/templates/reniec-consulta.php
+++ app/plantilla/templates/reniec-consulta.php
@@ -81,7 +81,7 @@
                                     </thead>
                                     <tbody class="text-center">
                                         <tr>
-                                        <td>{{data_reniec.NumDoc}}</td>
+                                        <td>{{data_reniec["NumDoc"]}}</td>
                                         <td>{{data_reniec.Apellido_p}}</td>
                                         <td>{{data_reniec.Apellido_m}}</td>
                                         <td>{{data_reniec.Nombres}}</td>
