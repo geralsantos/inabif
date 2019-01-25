339486cd360a66f3348ded6c76cd534ceb4ef00d
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-25 10:25:56 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -1629,8 +1629,9 @@ ini_set('session.gc_maxlifetime','1200');*/
       echo "geral";
       $wsdlurl = "https://ws5.pide.gob.pe/services/ReniecConsultaDni";
      
-      $página_inicio = file_get_contents('https://ws5.pide.gob.pe/Rest/Reniec/Consultar?nuDniConsulta=76934495&nuDniUsuario=45050812&nuRucUsuario=20507920722&password=45050812');
-      print_r($página_inicio);
+      $file_get_contents = file_get_contents('https://ws5.pide.gob.pe/Rest/Reniec/Consultar?nuDniConsulta=76934495&nuDniUsuario=45050812&nuRucUsuario=20507920722&password=45050812');
+      $xml_to_array = json_decode(json_encode(simplexml_load_string($file_get_contents)), 1);
+      print_r($xml_to_array);
      /* $client = new nusoap_client("https://ws5.pide.gob.pe/services/ReniecConsultaDni");
       $client->call("consultar",array("nuDniConsulta"=>76934495,"nuDniUsuario"=>45050812,"nuRucUsuario"=>20507920722,"password"=>45050812));
 
