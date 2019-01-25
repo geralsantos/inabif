0a6668d0bc2c98019204c15dd6fec44426cc3b68
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-01-25 11:22:26 -0500

    update

--- js/componentes/portada/reniec-consulta.js
+++ js/componentes/portada/reniec-consulta.js
@@ -121,7 +121,7 @@ Vue.component('reniec-consulta', {
                     if( response.body.data != undefined){
                         var x2js = new X2JS();
                         this.data_reniec = JSON.parse(JSON.stringify(x2js.xml_str2json(response.body.data)));
-                       
+                       console.log(this.data_reniec);
                         //alert(this.data_reniec.Envelope.Body.consultarResponse.return.datosPersona.apPrimer);
                         this.modal_lista = true;
                         this.isLoading = false;
