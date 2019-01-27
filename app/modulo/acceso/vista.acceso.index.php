bafb0586172916d49692b9aa486db09950558ecb
geral <geral_366@hotmail.com>
2019-01-27 15:18:48 -0500

    update

--- app/modulo/acceso/vista.acceso.index.php
+++ app/modulo/acceso/vista.acceso.index.php
@@ -1,4 +1,4 @@
-<div class="sufee-login d-flex align-content-center flex-wrap">
+<div style="background-color:yellow;" class="sufee-login d-flex align-content-center flex-wrap">
         <div class="container">
             <div class="login-content">
                 <div class="login-logo">
