031accefe22cd3d5a87763bbe65f90bc912b59e8
geral <geral_366@hotmail.com>
2019-01-27 17:24:55 -0500

    update

--- app/modulo/acceso/vista.acceso.index.php
+++ app/modulo/acceso/vista.acceso.index.php
@@ -1,6 +1,6 @@
 <div style="text-align: center; width: 100%;margin-bottom:-50px;margin-top:50px;">
          <br>
-	 <img src="../images/sector.png"  />
+	 <img src="../images/sector.png" style="width: 40%;" />
 <br>
 <br>
 	 <img src="../images/siger.png">
