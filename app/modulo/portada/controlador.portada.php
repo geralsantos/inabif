b317f426cfd55d159e20d96d297720beb9fa4638
geral <geral_366@hotmail.com>
2019-01-27 23:40:42 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -626,7 +626,7 @@ class portada extends App{
   }
   public function listar_usuarios(){
     $modelo = new modeloPortada();
-    $sql = "select nu.nombre,usu.* as nivel_nombre from usuarios usu, niveles_usuarios nu where usu.nivel(+)=nu.id order by nu.nombre asc";
+    $sql = "select nu.nombre as nivel_nombre,usu.*  from usuarios usu, niveles_usuarios nu where usu.nivel(+)=nu.id order by nu.nombre asc";
     $res = $modelo->executeQuery($sql );
     if ($res)
     {
