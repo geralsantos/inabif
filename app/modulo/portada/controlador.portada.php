37ab17ec69f60b58fa9f87f6697d670ca3cef5ef
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-02-04 11:19:14 -0500

    update

--- app/modulo/portada/controlador.portada.php
+++ app/modulo/portada/controlador.portada.php
@@ -336,18 +336,18 @@ class portada extends App{
       $tipo_centro_id = $_SESSION["usuario"][0]["TIPO_CENTRO_ID"];
       if ($tipo_centro_id == PPD) {
         $campo = "nd.Numero_Documento ";
-        $left_join = " left join CarCondicionIngreso nd on (nd.residente_id=re.id)";
-        $where_join = " ";
+        $left_join = " CarCondicionIngreso nd ";
+        $where_join = "and nd.residente_id(+)=re.id ";
 
       }else if($tipo_centro_id == PAM){
         $campo = "dci.numero_documento_ingreso ";
-        $left_join = " left join pam_datosCondicionIngreso dci on (dci.residente_id=re.id)";
-        $where_join = " ";
+        $left_join = "  pam_datosCondicionIngreso dci ";
+        $where_join = "and dci.residente_id(+)=re.id ";
 
       }else if($tipo_centro_id == NNA){
         $campo = "cir.Numero_Doc ";
-        $left_join = " left join NNACondicionIResidente cir on (cir.residente_id=re.id)";
-        $where_join = " ";
+        $left_join = " NNACondicionIResidente cir";
+        $where_join = "and cir.residente_id(+)=re.id ";
       }
         $nivel = $_SESSION["usuario"][0]["NIVEL"];
         if (SUPERVISOR == $nivel || USER_SEDE == $nivel) {
