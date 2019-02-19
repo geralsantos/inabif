9e473716263cef21cffb330ee2eb5b38f1f90743
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-02-19 14:15:27 -0500

    update

--- app/core/mysql.class.php
+++ app/core/mysql.class.php
@@ -1,1 +1,409 @@
-<?php /*** PHP Encode v1.0 by zeura.com ***/ $XnNhAWEnhoiqwciqpoHH=file(__FILE__);eval(base64_decode("aWYoIWZ1bmN0aW9uX2V4aXN0cygiWWl1bklVWTc2YkJodWhOWUlPOCIpKXtmdW5jdGlvbiBZaXVuSVVZNzZiQmh1aE5ZSU84KCRnLCRiPTApeyRhPWltcGxvZGUoIlxuIiwkZyk7JGQ9YXJyYXkoNjU1LDIzNiw0MCk7aWYoJGI9PTApICRmPXN1YnN0cigkYSwkZFswXSwkZFsxXSk7ZWxzZWlmKCRiPT0xKSAkZj1zdWJzdHIoJGEsJGRbMF0rJGRbMV0sJGRbMl0pO2Vsc2UgJGY9dHJpbShzdWJzdHIoJGEsJGRbMF0rJGRbMV0rJGRbMl0pKTtyZXR1cm4oJGYpO319"));eval(base64_decode(YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH)));eval(ZsldkfhGYU87iyihdfsow(YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH,2),YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH,1)));__halt_compiler();aWYoIWZ1bmN0aW9uX2V4aXN0cygiWnNsZGtmaEdZVTg3aXlpaGRmc293Iikpe2Z1bmN0aW9uIFpzbGRrZmhHWVU4N2l5aWhkZnNvdygkYSwkaCl7aWYoJGg9PXNoYTEoJGEpKXtyZXR1cm4oZ3ppbmZsYXRlKGJhc2U2NF9kZWNvZGUoJGEpKSk7fWVsc2V7ZWNobygiRXJyb3I6IEZpbGUgTW9kaWZpZWQiKTt9fX0=0a1bff3a44ddf5308cb2cf94e0f58737dc2fa5467Vrrb9pIEP9eqf/D1opkOyImSa+6O1JTOdgo6AimtulDUYQcswRUwNSPNujE/367fuG112BISE8p/VJnd3Zmdh6/mdmkenxcff3q9StrYrouuF7oH9sAPnhwNnBBV1Zfv/oX7wIw9+8mYwsM/Znlje0Z6Pcte+Z6jm95HA8iIgCOBndnQAQMJyt6Q2t1jZbaETlJljVF1/vtlm6g3fhn/NnVVENtqG30bTS6PHelBiRnf54LZ38Lp8LbdzzXVbVg7d35Gc/zXEPtdJSG0ZclQxI5vSWLDxCtM+Ai1CHQY2S7HlYkxYhJ7w/uZuYUYgrbsSaZLd+FDt5qdaTLVrPf63YJArw9x+ZCJNm9ue14oaqpVVCtzk0HzrxaLW03xrbGtVARsVoNVK4FDKqRehfWyHRc6Im+N/yLqUSaVRIF+LSMTRKwaxiBSXj2jGYRz9ibnrNYuXYlAZ2WPM8Z3/ke5OJFyTC0vqJp16qsVBLSaKGvfGkoQTCE7Mvyk5Wm1Gsb/aZiNK76JOtwTdJ1tZFmujI1fIAWx+iKARpXkiY1DEUD+KfAmKsjS2CZnjVCoSiryoMF50F8H0E+fXUAoDWyAas4ju2AS7kGWCAgopP6PfSuoeua95BLq7FM2K/Mmc0hrCC68kcfOgvu6Dv+r4IiyHTMqSt2eu02n9IA+YLQ58j1pjjC49vOHYg/Iz5pVQAYD7mA/KQeieQiKTxP8ATAgZ7vzEBEPYTIMNJkwpHslnDiQvpB03HMRZY8ZZVnsDXN0v58YHpQNj2TO/LMu4mJ7PzDnPjQRR8/R9CB2BCJsSzbn3lcRMHXT4E5G4BoMaSun64OBN4IrI7cwfa6CJeUUOVAFPpgg7hjL4gToQfQkZXNUruWPTUxO/LQ0HagaY1i1YDpgqNvEMmtB/chdUrpJYghRyGgFljEGWddP9ASLV3kzoXa3bAE2S06h+XkyWN1K6S+S4qNkC4s+HylaErWItjKxVcODL/5xjjW0YYoimxHNQBOIzZPlbENkoyMwgbmQR8tHSRnc3eNoz/PkBu7/ZmP0iXQjP+QlRAb/z1Svoauk9gV1EARrUhSXlAvkjgrTVrorGXOe5HhgdSRwToHboScDG0ebwiaFSw59s9GkF65xEphi6H1FFI5qici6qbU1hX6XfLnqGeWCaishxQMkAWoIka5fUCXl4UuN2e3vxxgkA6PxRjM5fT2aWAGqXNAmr0gjQsn0PLI5oVAF/Sz7Qygc7kQ2VQorlBDV9poYALHoKmp1ynwuMij0loA2jK51oPQSy3s5Yp6yUzbTzFfpvweh86bIHYIT6uajEamy6/YfhHVxeps2RzdlJ9rRo5SSUGU3983L/ZfkjKSLhXjs6J0qIJoYsJig76jg4CJFs/RIhOEa7zyNljZZ2UsXRV3zNMnq4YvIFXHMxc6XsHwPTFdbzwQGSZ4AkjFdLZLzobZKotbHV3RDNDqGCq+PpnBaeJ/4MLNZyOxi43ZGlQAneRT2BdTWIyHgIvuQlyEeh5HWkgrMBUmw6koxrMcGBd+7zPRdQVW6ChfjE9SO6/6knyo2QQ/hdPBDvNB1rSrKYGSTtiErufMbRf/59n+fA4dDhPzFaapNK4khn8jikMT2YdqY6qdA4kM8Ow+Htc4lgnyW2DQLPEV/Tu5vj6RZTAanf9Rm45rrsvygKEoV4w8BAoIa2v1OhXjwzTZFLMWTEQ54hRGcEhA4g0B3RNFS0/RQbIeqoR2csG/RTderh8v35EX9+TFTinsywnz0E4XnFyWxLdr0x3/sGOUQ06a+NOZm8Bd8hqwf6iLPT6ezif2AMV9hU304VPOz1BEKgjsk4bAC/Z92IZq9k+yvA3HcDJY8wq0ubOktKnI4SFf1JkUNazl2tKixnRzbdi2Od1mbNu5O10Pz889vG01vtHRvbgvzAP8HrCZ0uYRgbCf7CHmOHoCoZ/vHdQYoIEuP+LRk+wNnM69RcSYzwYe7jliilAkT2ktwq3CnEiyAsRMsnnh0xuWkPomDg4flhgZYlXSSvMf2GO2RgB5uF6qN8gDzGZ82YASabOG8Eaz6kaQKoapXYBoy0n5UXi0/cS8x6m5SJ26uJsmdZFUorzE9ztKfL+zRFXbTWJGIJqvC7bfbtCnMLZ+zYvF9q8WBTVqQ5XKnVkWI0QE6VSMuHfmpXA34lEWeBHbm9stUDeuOgwIvsDdAjBk54w48rQWlXqnUFcKrq2xUlTmHmWliMe+rBRXYgYEX4+zUqzrZiutb4GE2N7Jw9iTNUXpt6+99kX6R3Ra+6Ro2zdIh3bo0A6tb4eeqRcqVwifZT7bckJ7qvp3qIDlKuBO6P7/B/UBAvXwD4Y69mecpTEKg8KHH0PrdRr4730M6bKt7A5AL+sBt6SVM78ULnh03fz6JqNqgFxweHH7XV/cthKwPPzqZCdMzP8Vx2+XsGXfpfafs8/4HLHtY8TTJm9exiF/i/J3+R8=
+<?php /**/
+
+class MySQL extends PDO
+{
+
+  public function __construct() {
+
+    $db1 = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.19.0.35)(PORT = 1521)))(CONNECT_DATA=(SID=xe)))" ;
+        $host = "172.19.0.35";
+        $dbname = "orcl";
+        $dbuser = "INABIF_UPP";
+        $userpass = "UPP";
+        $port= 1521;
+         //parent::__construct("oci:dbname=//$host:$port/$dbname;charset=utf8",$dbuser,$userpass);
+         parent::__construct("oci:dbname=$db1".";charset=UTF8",$dbuser,$userpass);
+
+
+    try {
+
+      parent::setAttribute(parent::ATTR_ERRMODE, parent::ERRMODE_EXCEPTION);
+
+      parent::setAttribute(parent::ATTR_DEFAULT_FETCH_MODE, parent::FETCH_ASSOC);
+
+      //parent::exec("SET CHARACTER SET utf8");
+
+    } catch (PDOException $e) {
+
+        echo 'Error BD: ' . $e->getMessage();
+
+      }
+
+    }
+
+
+    public function executeQuery($query, $params=NULL){
+
+      try{
+
+        $stmt = parent::prepare($query);
+
+        if($stmt->execute($params)){
+
+          return $stmt->fetchAll();
+
+        }else{
+
+          return array();
+
+        }
+
+      } catch (PDOException $e) {
+
+        echo 'Error BD: ' . $e->getMessage();
+
+      }
+
+    }
+
+    public function updateData($tabla, $values, $where){
+      if(count($values)>0 and count($where)>0){
+          $query = 'UPDATE ' . $tabla . ' SET ';
+          $params = array();
+          $coma = '';
+          foreach($values as $key => $val){
+              $query .= $coma.$key .' = :set_' . $key;
+              $params[':set_' . $key] = $val;
+              $coma = ',';
+          }
+          $query .= ' WHERE ';
+          $and = '';
+          foreach($where as $key => $val){
+              if($val==='NOT NULL'){
+                  $query .= $and.' '.$key.' IS NOT NULL';
+              }else{
+                  (is_null($val))? $query .= $and.$key .' <=> :whe_' . $key :  $query .= $and.$key .' = :whe_' . $key;
+                  $params[':whe_' . $key] = $val;
+              }
+              $and = ' AND ';
+          }
+          $stmt = parent::prepare($query);
+          $stmt->execute($params);
+          if($stmt->rowCount()>0){
+              return TRUE;
+          }else{
+              return FALSE;
+          }
+      }else{
+          return FALSE;
+      }
+    }
+    public function updateDataAll($tabla, $values, $where=array()){
+      if(count($values)>0 and count($where)>0){
+          $query = 'UPDATE ' . $tabla . ' SET ';
+          $params = array();
+          $coma = '';
+          foreach($values as $key => $val){
+              $query .= $coma.$key .' = :set_' . $key;
+              $params[':set_' . $key] = $val;
+              $coma = ',';
+          }
+          $query .= ' WHERE ';
+          $and = '';
+          foreach($where as $key => $val){
+              if($val[1]==='NOT NULL'){
+                  $query .= $and.' '.$key.' IS NOT NULL';
+              }else{
+                  (is_null($val[1]))? $query .= $and.$key .' <=> :whe_' . $key :  $query .= $and.$val[0] .' = :whe_' . $key;
+                  $params[':whe_' . $key] = $val[1];
+              }
+              $and = ' AND ';
+          }
+          $stmt = parent::prepare($query);
+          $stmt->execute($params);
+          if($stmt->rowCount()>0){
+              return TRUE;
+          }else{
+              return FALSE;
+          }
+      }else{
+          return FALSE;
+      }
+    }
+    public function selectData($tabla, $where=array(), $orderBy=''){
+      $query = 'SELECT * FROM ' . $tabla;
+      if(count($where)>0){
+          $query .= ' WHERE ';
+          $and = '';
+          $params = array();
+          foreach($where as $key => $val){
+              if($val==='NOT NULL'){
+                  $query .= $and.' '.$key.' IS NOT NULL';
+              }else{
+                  (is_null($val))? $query .= $and.$key .' <=> :' . $key :  $query .= $and.$key .' = :' . $key;
+                  $params[':' . $key] = $val;
+              }
+              $and = ' AND ';
+          }
+      }
+      if($orderBy!=''){ $query .= ' ORDER BY '.$orderBy; }
+      $stmt = parent::prepare($query);
+      $stmt->execute($params);
+      return $stmt->fetchAll();
+    }
+    public function selectDataAll($tabla, $where=array(), $orderBy=''){
+      $query = 'SELECT * FROM ' . $tabla;
+      if(count($where)>0){
+          $query .= ' WHERE ';
+          $and = '';
+          $params = array();
+          foreach($where as $key => $val){
+              if($val[1]==='NOT NULL'){
+                  $query .= $and.' '.$key.' IS NOT NULL';
+              }else if($val[1]==='BETWEEN'){
+                $query .= $and.' '.$val[0].' BETWEEN "'.$val[2].'" AND "'.$val[3].'" ';
+              }else{
+                  (is_null($val[1]))? $query .= $and.$key .' <=> :' . $key :  $query .= $and.$val[0] .' = :' . $key;
+                  $params[':' . $key] = $val[1];
+              }
+              $and = ' AND ';
+          }
+      }
+      if($orderBy!=''){ $query .= ' ORDER BY '.$orderBy; }
+      $stmt = parent::prepare($query);
+      $stmt->execute($params);
+      return $stmt->fetchAll();
+    }
+    public function insertData($tabla, $values, $lastid="") {
+        if(count($values)>0){
+            $query = 'INSERT INTO '.$tabla;
+            $queryKeys = '';
+            $queryKeys .= 'Id, ';
+            $queryValues = '';
+            if ($lastid) {
+                $queryValues .= $lastid.",";
+            }else{
+                $queryValues .= "seq_".$tabla.'.NEXTVAL, ';
+            }
+
+            $params = array();
+            $coma = '';
+            foreach($values as $key => $val){
+                $queryKeys .= $coma.$key;
+                if (strpos(strtoupper($key),"FECHA")!==false) {
+                    $queryValues .= $coma." to_date('".$val."','YYYY-MM-DD hh24:mi:ss') ";
+                }else{
+                    $params[':'.$key] = $val;
+                    $queryValues .= $coma.':'.$key;
+                }
+                $coma = ',';
+            }
+            $query .= '('.$queryKeys.') VALUES ('.$queryValues.')';
+            if($val=="prueba"){
+                echo $query;
+            }
+            $stmt = parent::prepare($query);
+            $stmt->execute($params);
+            if($stmt->rowCount()>0){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }else{
+            return FALSE;
+        }
+    }
+    public function insertDataMasivo($tabla,$columns, $values=array()) {
+        if(count($values)>0){
+            $query = 'INSERT INTO '.$tabla;
+            $query .= '('.implode(',',$columns).') VALUES '.implode(',',$values).'';
+            $stmt = parent::prepare($query);
+            $stmt->execute();
+            if($stmt->rowCount()>0){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }else{
+            return FALSE;
+        }
+    }
+    public function selectRowData($tabla, $fields, $where=array()){
+        if(count($where)>0){
+            $query = 'SELECT '.$fields.' FROM ' . $tabla;
+            $query .= ' WHERE ';
+            $and = '';
+            $params = array();
+            foreach($where as $key => $val){
+                if($val==='NOT NULL'){
+                    $query .= $and.' '.$key.' IS NOT NULL';
+                }else{
+                    (is_null($val))? $query .= $and.$key .' <=> :' . $key :  $query .= $and.$key .' = :' . $key;
+                    $params[':' . $key] = $val;
+                }
+                $and = ' AND ';
+            }
+            $stmt = parent::prepare($query);
+            $stmt->execute($params);
+            return $stmt->fetch();
+        }else{
+            return FALSE;
+        }
+    }
+    public function selectRowDataAll($tabla, $fields, $where=array(), $groupBy=array(), $orderBy=array()){
+        if(!empty($tabla)){
+            if (!empty($fields)) {
+              $field = array();
+              foreach ($fields as $key => $value) {
+                $field[] = $value;
+              }
+              $field = empty($field)?'*':implode(',',$field);
+            }
+            $query = 'SELECT '.$field.' FROM ' . $tabla;
+            $params = array();
+            if (!empty($where)) {
+              $query .= ' WHERE ';
+              $and = '';
+              foreach($where as $key => $val){
+                  if($val[1]==='NOT NULL'){
+                      $query .= $and.' '.$key.' IS NOT NULL';
+                  }else if($val[1]==='BETWEEN'){
+                    $query .= $and.' '.$val[0].' BETWEEN "'.$val[2].'" AND "'.$val[3].'" ';
+                  }else if($val[1]==='>='){
+                    $query .= $and.' '.$val[0].' >= "'.$val[2].'"';
+                  }else if($val[1]==='<='){
+                    $query .= $and.' '.$val[0].' <= "'.$val[2].'"';
+                  }else if($val[1]==='OR'){
+                    $query .= $and.' '.$val[0].' = "'.$val[2].'" OR '.$val[0].' = "'.$val[3].'"';
+                  }else{
+                      (is_null($val[1]))? $query .= $and.$key .' <=> :' . $key :  $query .= $and.$val[0] .' = :' . $key;
+                      $params[':' . $key] = $val[1];
+                  }
+                  $and = ' AND ';
+              }
+            }
+            if (!empty($groupBy)) {
+              $grp = array();
+              foreach ($groupBy as $key => $value) {
+                $grp[]= $value;
+              }
+              $groupBy=" group by ".implode(',',$grp);
+            }else {
+              $groupBy = '';
+            }
+            if (!empty($orderBy)) {
+              $grp = array();
+              foreach ($orderBy as $key => $value) {
+                $grp[]= $value;
+              }
+              $orderBy=" order by ".implode(',',$grp);
+            }else {
+              $orderBy = '';
+            }
+            $stmt = parent::prepare($query.$groupBy.$orderBy);
+            $stmt->execute($params);
+            return $stmt->fetchAll();
+        }else{
+            return FALSE;
+        }
+    }
+    public function selectRowDataAllSQLSERVER($tabla, $fields, $where=array(), $groupBy=array()){
+        if(!empty($tabla)){
+            if (!empty($fields)) {
+              $field = array();
+              foreach ($fields as $key => $value) {
+                $field[] = $value;
+              }
+              $field = empty($field)?'*':implode(',',$field);
+            }
+            $query = 'SELECT '.$field.' FROM ' . $tabla;
+            $params = array();
+            if (!empty($where)) {
+              $query .= ' WHERE ';
+              $and = '';
+              foreach($where as $key => $val){
+                  if($val==='NOT NULL'){
+                      $query .= $and.' '.$key.' IS NOT NULL';
+                  }else{
+                      (is_null($val))? $query .= $and.$key .' <=> :' . $key :  $query .= $and.$key .' = :' . $key;
+                      $params[':' . $key] = $val;
+                  }
+                  $and = ' AND ';
+              }
+            }
+
+            if (!empty($groupBy)) {
+              $grp = array();
+              foreach ($groupBy as $key => $value) {
+                $grp[]= $value;
+              }
+              $groupBy=" group by ".implode(',',$grp);
+            }else {
+              $groupBy = '';
+            }
+
+            $stmt = parent::prepare($query.$groupBy);
+            $stmt->execute($params);
+            return $stmt->fetchAll();
+        }else{
+            return FALSE;
+        }
+    }
+    public function deleteDataNoWhere($tabla) {
+            $query = 'TRUNCATE TABLE ' . $tabla;
+            $params = array();
+            $stmt = parent::prepare($query);
+            $stmt->execute($params);
+            if($stmt->rowCount()>0){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+
+    }
+    public function deleteData($tabla, $where) {
+        if(count($where)>0){
+            $query = 'DELETE FROM ' . $tabla;
+            $query .= ' WHERE ';
+            $and = '';
+            $params = array();
+            foreach($where as $key => $val){
+                if($val==='NOT NULL'){
+                    $query .= $and.' '.$key.' IS NOT NULL';
+                }else{
+                    (is_null($val))? $query .= $and.$key .' <=> :' . $key :  $query .= $and.$key .' = :' . $key;
+                    $params[':' . $key] = $val;
+                }
+                $and = ' AND ';
+                $params[':' . $key] = $val;
+            }
+            $stmt = parent::prepare($query);
+            $stmt->execute($params);
+            if($stmt->rowCount()>0){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }else{
+            return FALSE;
+        }
+    }
+    public function deleteDataAll($tabla, $where) {
+        if(count($where)>0){
+            $query = 'DELETE FROM ' . $tabla;
+            $query .= ' WHERE ';
+            $and = '';
+            $params = array();
+            foreach($where as $key => $val){
+                if($val[1]==='NOT NULL'){
+                    $query .= $and.' '.$key.' IS NOT NULL';
+                }else{
+                    (is_null($val[1]))? $query .= $and.$key .' <=> :' . $key :  $query .= $and.$val[0] .' = :' . $key;
+                    $params[':' . $key] = $val[1];
+                }
+                $and = ' AND ';
+                $params[':' . $key] = $val[1];
+            }
+            $stmt = parent::prepare($query);
+            $stmt->execute($params);
+            if($stmt->rowCount()>0){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }else{
+            return FALSE;
+        }
+    }
+}
+
