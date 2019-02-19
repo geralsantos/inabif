3fc5fde2d599019e6031cc6d0c96583149d87f4b
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-02-19 15:52:29 -0500

    update

--- js/componentes/portada/pam-datos-identificacion-residente.js
+++ js/componentes/portada/pam-datos-identificacion-residente.js
@@ -344,8 +344,9 @@ var pam_datos_identificacion_residente = {
                     this.distrito_nacimiento_id = response.body.atributos[0]["DISTRITO_NACIMIENTO_ID"];
                     this.Sexo = response.body.atributos[0]["SEXO"];
                     this.Edad = response.body.atributos[0]["EDAD"];
-                    this.Fecha_Nacimiento = isempty(response.body.atributos[0]["FECHA_NACIMIENTO"])?null:moment(response.body.atributos[0]["FECHA_NACIMIENTO"],'DD-MMM-YY').format("YYYY-MM-DD");
-                    console.log(moment([1945, 5, null]));
+                    this.Fecha_Nacimiento = moment(moment(response.body.atributos[0]["FECHA_NACIMIENTO"],'DD-MMM').format("DD-MM")+"-"+(moment().format("YYYY"))).subtract(this.Edad,'years').format("YYYY-MM-DD");
+                    //this.Fecha_Nacimiento = isempty(response.body.atributos[0]["FECHA_NACIMIENTO"])?null:moment(response.body.atributos[0]["FECHA_NACIMIENTO"],'DD-MMM-YY').format("YYYY-MM-DD");
+                    console.log(this.Fecha_Nacimiento);
                     this.Lengua_Materna = response.body.atributos[0]["LENGUA_MATERNA"];
                     this.id = response.body.atributos[0]["RESIDENTE_ID"];
                 }
