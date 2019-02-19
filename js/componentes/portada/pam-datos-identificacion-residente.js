89c3513662ccaf33a0f4072df65efa810e475421
SYS Geral Poma Santos <gpoma@peruvian.local>
2019-02-19 14:05:56 -0500

    update

--- js/componentes/portada/pam-datos-identificacion-residente.js
+++ js/componentes/portada/pam-datos-identificacion-residente.js
@@ -344,7 +344,7 @@ var pam_datos_identificacion_residente = {
                     this.Sexo = response.body.atributos[0]["SEXO"];
                     this.Edad = response.body.atributos[0]["EDAD"];
                     this.Fecha_Nacimiento = isempty(response.body.atributos[0]["FECHA_NACIMIENTO"])?null:moment(response.body.atributos[0]["FECHA_NACIMIENTO"],'DD-MMM-YY').format("YYYY-MM-DD");
-                    console.log((this.Fecha_Nacimiento).toDate());
+                    console.log(moment(response.body.atributos[0]["FECHA_NACIMIENTO"],'DD-MMM-YY').toDate());
 
                     this.Lengua_Materna = response.body.atributos[0]["LENGUA_MATERNA"];
                     this.id = response.body.atributos[0]["RESIDENTE_ID"];
